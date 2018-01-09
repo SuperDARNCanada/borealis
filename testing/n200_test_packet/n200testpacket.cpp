eb4d27b30c8a2571a66bb8054b1c0d084bd9dda6
Keith Kotyk <keithkotyk@gmail.com>
2018-01-09 11:07:45 -0600

    Modified test packet for more accurate driver test
    I modified the test packet to use driver options as well as create real pulse samples and use a real pulse sequence.

--- testing/n200_test_packet/n200testpacket.cpp
+++ testing/n200_test_packet/n200testpacket.cpp
@@ -1,38 +1,91 @@
 #include <zmq.hpp>
-#include "utils/protobuf/driverpacket.pb.h"
 #include <thread>
 #include <unistd.h>
 #include <iostream>
+#include <complex>
+#include "utils/protobuf/driverpacket.pb.h"
+#include "utils/driver_options/driveroptions.hpp"
+
+
+std::vector<std::complex<float>> make_pulse(DriverOptions &driver_options){
+    auto amp = 1.0/sqrt(2.0);
+    auto pulse_len = 300.0 * 10e-6;
+    auto tx_rate = driver_options.get_tx_rate();
+    auto num_samps_per_antenna = tx_rate * pulse_len;
+    std::vector<double> tx_freqs = {1e6};
+
+    auto default_v = std::complex<float>(0.0,0.0);
+    std::vector<std::complex<float>> samples(num_samps_per_antenna,default_v);
+
+
+    for (auto j=0; j< num_samps_per_antenna; j++) {
+        auto nco_point = std::complex<float>(0.0,0.0);
+
+        for (auto freq : tx_freqs) {
+          auto sampling_freq = 2 * M_PI * freq/tx_rate;
+
+          auto radians = fmod(sampling_freq * j, 2 * M_PI);
+          auto I = amp * cos(radians);
+          auto Q = amp * sin(radians);
+
+          nco_point += std::complex<float>(I,Q);
+        }
+        samples[j] = nco_point;
+    }
+
+      auto ramp_size = int(10e-6 * tx_rate);
+
+      for (auto j=0; j<ramp_size; j++){
+        auto a = ((j+1)*1.0)/ramp_size;
+        samples[j] *= std::complex<float>(a,0);
+      }
+
+      for (auto j=num_samps_per_antenna-1;j>num_samps_per_antenna-1-ramp_size;j--){
+        auto a = ((j+1)*1.0)/ramp_size;
+        samples[j] *= std::complex<float>(a,0);
+      }
+
+
+    return samples;
+}
+
 int main(int argc, char *argv[]){
+
+    DriverOptions driver_options;
 
     driverpacket::DriverPacket dp;
     zmq::context_t context(1);
     zmq::socket_t socket(context, ZMQ_PAIR);
-    socket.connect("ipc:///tmp/feeds/0");
+    socket.connect(driver_options.get_radar_control_to_driver_address());
 
-    for (int j=0; j<1; j++){
+
+    auto pulse_samples = make_pulse(driver_options);
+    for (int j=0; j<driver_options.get_main_antenna_count(); j++){
         dp.add_channels(j);
-        auto samples = dp.add_samples();
+        auto samples = dp.add_channel_samples();
 
-        for (int k=0; k<1600; k++){
-            samples->add_real(k);
-            samples->add_imag(k);
+        for (auto &sm : pulse_samples){
+            samples->add_real(sm.real());
+            samples->add_imag(sm.imag());
         }
     }
 
     bool SOB, EOB = false;
+    std::vector<int> pulse_seq = {0,9,12,20,22,26,27};
+
+    auto first_time = true;
     while (1){
-        for (int i=0; i<8; i++){
+        for (auto &pulse : pulse_seq){
             std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
-            if (i == 0){
+            if (pulse == pulse_seq.front()){
                 SOB = true;
             }
             else{
                 SOB = false;
             }
 
-            if (i == 7){
+            if (pulse == pulse_seq.back()){
                 EOB = true;
             }
             else{
@@ -41,20 +94,11 @@ int main(int argc, char *argv[]){
             std::cout << SOB << " " << EOB <<std::endl;
             dp.set_sob(SOB);
             dp.set_eob(EOB);
-            dp.set_txrate(1e6);
-            dp.set_timetosendsamples(i * 1500);
+            dp.set_txrate(driver_options.get_tx_rate());
+            dp.set_timetosendsamples(pulse * 1500);
             dp.set_txcenterfreq(12e6);
             dp.set_rxcenterfreq(14e6);
             dp.set_numberofreceivesamples(1000000);
-
-            for (int j=0; j<1; j++){
-                dp.set_channels(j,j);
-
-                for (int k=0; k<1600; k++){
-                    dp.mutable_samples(j)->set_real(k,1.0);
-                    dp.mutable_samples(j)->set_imag(k,1.0);
-                }
-            }
 
             std::string msg_str;
             dp.SerializeToString(&msg_str);
@@ -70,6 +114,11 @@ int main(int argc, char *argv[]){
 
             std::cout << "send time(us) = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() <<std::endl;
             std::cout << "send time(ns) = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() <<std::endl;
+
+            if (first_time == true) {
+                dp.clear_channel_samples();
+                first_time = false;
+            }
 
         }
         sleep(1);
