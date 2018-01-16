5abde0845178c7d73cf9b688a4cb721633d4f594
mardet987 <marci.detwiller@gmail.com>
2018-01-16 21:30:06 +0000

    Basic sys path change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ import sphinx_rtd_theme
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
-BOREALIS_PROJECT_PATH = '../..'
+BOREALIS_PROJECT_PATH = '../'
 sys.path.append(BOREALIS_PROJECT_PATH)
 sys.path.append(BOREALIS_PROJECT_PATH + '/experiments')
 
