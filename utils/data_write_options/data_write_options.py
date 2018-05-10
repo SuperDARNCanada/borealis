8f71ec87a8a0b5ebb0c4c93286f7be0b201083fa
Kevin Krieger <kevin.krieger@usask.ca>
2018-05-10 17:12:28 +0000

    Added basic data writing functionality in data_write.py. Other changes include a comment in the protobuf packet 'processeddata.proto', updates to the data write options class, and a new line in the config file to point to a data directory.

--- utils/data_write_options/data_write_options.py
+++ utils/data_write_options/data_write_options.py
@@ -1,8 +1,11 @@
 import json
 import os
+
+
 def ascii_encode_dict(data):
     ascii_encode = lambda x: x.encode('ascii')
     return dict(map(ascii_encode, pair) for pair in data.items())
+
 
 class DataWriteOptions(object):
     """
@@ -15,23 +18,35 @@ class DataWriteOptions(object):
         if not os.environ["BOREALISPATH"]:
             raise ValueError("BOREALISPATH env variable not set")
         config_path = os.environ["BOREALISPATH"] + "/config.ini"
-        with open(config_path,'r') as f:
-            raw_config = json.load(f,object_hook=ascii_encode_dict)
-
-
-        self._rx_dsp_to_data_write_address = raw_config["rx_dsp_to_data_write_address"]
+        try:
+            with open(config_path, 'r') as config_data:
+                raw_config = json.load(config_data, object_hook=ascii_encode_dict)
+        except IOError:
+            errmsg = 'Cannot open config file at {0}'.format(config_path)
+            raise IOError(errmsg)
+        self._dsp_to_dw_identity = raw_config["dsp_to_dw_identity"]
         self._debug_file = raw_config["filter_outputs_debug_file"]
+        self._data_directory = raw_config["data_directory"]
+        self._site_id = raw_config["site_id"]
+        self._rx_sample_rate = float(raw_config["rx_sample_rate"])
+        self._max_usrp_dac_amplitude = float(raw_config["max_usrp_dac_amplitude"])
+        self._pulse_ramp_time = float(raw_config["pulse_ramp_time"])
+        self._tr_window_time = float(raw_config["tr_window_time"])
+        self._output_sample_rate = float(raw_config["third_stage_sample_rate"])
+        self._atten_window_time_start = float(raw_config["atten_window_time_start"])  # s
+        self._atten_window_time_end = float(raw_config["atten_window_time_end"])  # s
+        self._router_address = raw_config["router_address"]
 
     @property
-    def rx_dsp_to_data_write_address(self):
+    def dsp_to_dw_identity(self):
         """
-        Gets the address used for the rx dsp to data write socket.
+        Gets the identity used for the dsp to data write socket.
 
 
-        :returns: The address to bind to for dsp/data_write socket.
+        :returns: The identity to use for dsp/data_write socket.
         :rtype: str
         """
-        return self._rx_dsp_to_data_write_address
+        return self._dsp_to_dw_identity
 
     @property
     def debug_file(self):
@@ -43,7 +58,105 @@ class DataWriteOptions(object):
         """
         return self._debug_file
 
+    @property
+    def data_directory(self):
+        """
+        Gets the location of the directory to place data files in.
+        
+        :returns:  Data directory location
+        :rtype:    str
+        """
+        return self._data_directory
 
+    @property
+    def site_id(self):
+        """
+        Gets the 3 letter radar code of this radar.
+        
+        :return:    3 letter radar code 
+        :rtype:     str
+        """
+        return self._site_id
+
+    @property
+    def rx_sample_rate(self):
+        """
+        Gets the rx sample rate in samples per second.
+
+        :return:    rx sample rate
+        :rtype:     float
+        """
+        return self._rx_sample_rate
+
+    @property
+    def max_usrp_dac_amplitude(self):
+        """
+        Gets the maximum usrp dac amplitude, which is a value usually between -1 and 1
+
+        :return:    maximum dac amplitude of USRP units 
+        :rtype:     float
+        """
+        return self._max_usrp_dac_amplitude
+
+    @property
+    def pulse_ramp_time(self):
+        """
+        Gets the ramp-up and ramp-down time of the RF pulses in seconds
+
+        :return:    ramp-up/ramp-down time of the RF pulse in seconds.
+        :rtype:     float
+        """
+        return self._pulse_ramp_time
+
+    @property
+    def tr_window_time(self):
+        """
+        Gets the time before and after the RF pulse that the TR signal is active for in seconds. 
+
+        :return:    time before and after the RF pulse that TR signal is active for in seconds
+        :rtype:     float
+        """
+        return self._tr_window_time
+
+    @property
+    def output_sample_rate(self):
+        """
+        Gets the sample rate of the output of the final filter stage in samples per second
+        
+        :return:    sample rate of the output of the final filter stage in samples per second
+        :rtype:     float
+        """
+        return self._output_sample_rate
+
+    @property
+    def atten_window_time_start(self):
+        """
+        Gets the time before the RF pulse that the atten signal is active for in seconds.
+
+        :return:    time before the RF pulse that the atten signal is active for in seconds.
+        :rtype:     float
+        """
+        return self._atten_window_time_start
+
+    @property
+    def atten_window_time_end(self):
+        """
+        Gets the time after the RF pulse that the atten signal is active for in seconds.
+
+        :return:    time after the RF pulse that the atten signal is active for in seconds.
+        :rtype:     float
+        """
+        return self._atten_window_time_end
+
+    @property
+    def router_address(self):
+        """
+        Gets the socket address of the router that routes interprocess messages
+
+        :return:    socket address of the router that routes interprocess messages
+        :rtype:     str
+        """
+        return self._router_address
 
 if __name__ == '__main__':
     DataWriteOptions()
