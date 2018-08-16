2849fc2a4c6b7fa59862a9d9529ce231cca9cfa9
radar <superdarn@gmail.com>
2018-08-16 16:27:42 -0600

    Changed the timing macro to work at runtime or compile time

--- utils/shared_macros/shared_macros.hpp
+++ utils/shared_macros/shared_macros.hpp
@@ -15,22 +15,35 @@
 #define COLOR_CYAN(x)    "\033[36m"<< x << "\033[0m"
 #define COLOR_WHITE(x)   "\033[37m"<< x << "\033[0m"
 
+#define RUNTIME_MSG(x) do {std::cout << x << std::endl;} while (0)
+
+
 #ifdef DEBUG
 #define DEBUG_MSG(x) do {std::cerr << x << std::endl;} while (0)
-
-#define TIMEIT_IF_DEBUG(msg,x) do { auto time_start = std::chrono::steady_clock::now();            \
-                                    x;                                                             \
-                                    auto time_end = std::chrono::steady_clock::now();              \
-                                    auto time_diff = std::chrono::duration_cast                    \
-                                                                        <std::chrono::microseconds>\
-                                                    (time_end - time_start).count();               \
-                                    DEBUG_MSG(msg << COLOR_MAGENTA(time_diff) << "us"); } while (0)
+#define TIMEIT_IF_TRUE_OR_DEBUG(truth, msg, x) do {                                                                          \
+      auto time_start = std::chrono::steady_clock::now();                      \
+      x;                                                                       \
+      auto time_end = std::chrono::steady_clock::now();                        \
+      auto time_diff = std::chrono::duration_cast <std::chrono::microseconds>  \
+                                        (time_end - time_start).count();       \
+      DEBUG_MSG(msg << COLOR_MAGENTA(time_diff) << "us");                      \
+    } while (0) 
 #else
 #define DEBUG_MSG(x)
-#define TIMEIT_IF_DEBUG(msg,x) do {x;} while(0)
+#define TIMEIT_IF_TRUE_OR_DEBUG(truth, msg, x) do{                                                                          \
+      if(truth) {                                                              \
+        auto time_start = std::chrono::steady_clock::now();                    \
+        x;                                                                     \
+        auto time_end = std::chrono::steady_clock::now();                      \
+        auto time_diff = std::chrono::duration_cast <std::chrono::microseconds>\
+                                          (time_end - time_start).count();     \
+        RUNTIME_MSG(msg << COLOR_MAGENTA(time_diff) << "us");}                 \
+      else {                                                                   \
+        x;                                                                     \
+      }                                                                        \
+    } while (0)  
+//#define TIMEIT_IF_DEBUG(msg,x) do {x;} while(0)
 #endif
-
-#define RUNTIME_MSG(x) do {std::cout << x << std::endl;} while (0)
 
 
 #endif
