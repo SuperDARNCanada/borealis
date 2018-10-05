600bfe3e4255ddb518043b10d77ca579c1818531
radar <superdarn@gmail.com>
2018-10-05 12:09:14 -0600

    Correlating tx and rx samps, finding phase offset
    Updated test_beamforming.py to find the phase offset between tx and rx
    samples for each pulse in the decimated samples data. The phase offset
    for each pulse's sample was consistent (error of approx 0.0003 rads,
    0.02 degrees max between pulses). At 10.5 MHz this corresponds to 5 ps
    error in the ADC so this is good.
    
    Corrected a problem with a floating point error in num_samples in
    write_samples_to_file in samples_building.py that resulted in a missed
    sample in the sequence.

--- testing/dsp_testing/test_beamforming.py
+++ testing/dsp_testing/test_beamforming.py
@@ -51,16 +51,37 @@ for antk, antv in decimated_sequences.iteritems():
 ant_samples = np.array(ant_samples)
 combined_tx_samples = np.sum(ant_samples,axis=0)
 
-fig, (ax1,ax2,ax3) = plt.subplots(3,1)
+fig, (ax1,ax2,ax3,ax4) = plt.subplots(4,1, sharex=True)
 
 
 #print(combined_tx_samples.shape, main_beams.shape)
 
-corr = np.correlate(ant_samples[0],main_beams[0],mode='same')
+corr = np.correlate(combined_tx_samples,main_beams[0],mode='full')
+max_correlated_index = np.argmax(np.abs(corr))
+#print('Max index {}'.format(max_correlated_index))
+correlated_offset = max_correlated_index - len(combined_tx_samples) +1
+#print('Correlated offset = {}'.format(correlated_offset))
+zeros_offset = np.array([0.0]*(correlated_offset))
+#print('Shapes of arrays:{}, {}'.format(zeros_offset.shape, ant_samples[0].shape))
+aligned_ant_samples = np.concatenate((zeros_offset, combined_tx_samples))
+zeros_extender = np.array([0.0]*(len(main_beams[0])-len(aligned_ant_samples)))
+aligned_ant_samples = np.concatenate((aligned_ant_samples,zeros_extender))
+pulse_points = (aligned_ant_samples != 0.0)
+tx_pulse_samples = aligned_ant_samples[pulse_points]
+rx_pulse_samples = main_beams[0][pulse_points]
+samples_diff = np.subtract(rx_pulse_samples, tx_pulse_samples)
+phase_offsets = np.angle(samples_diff)
+print('Phase offsets {}'.format(phase_offsets))
+
+for pulse_index, offset_time in enumerate(pulse_offset_errors):
+    if offset_time != 0.0
+        phase_rotation = offset_time * 2 * cmath.pi * 
+        phase_offsets[pulse_index] = phase_offsets[pulse_index] * cmath.exp()
 
 ax1.plot(np.arange(len(ant_samples[0])),np.abs(ant_samples[0]))
-ax2.plot(np.arange(len(main_beams[0])),np.abs(main_beams[0]))
-ax3.plot(np.arange(len(corr)),np.abs(corr))
+ax2.plot(np.arange(len(aligned_ant_samples)),np.abs(aligned_ant_samples))
+ax3.plot(np.arange(len(main_beams[0])),np.abs(main_beams[0]))
+ax4.plot(np.arange(len(corr)),np.abs(corr))
 
 plt.show()
 
