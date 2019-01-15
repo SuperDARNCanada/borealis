248efed0b4769c2210cf0ebd291c00f09ee6514a
Keith Kotyk <keithkotyk@gmail.com>
2019-01-15 16:05:54 -0600

    Changes resulting from x300 test
    I changed the driver to not continually recreate the stream. In the
    current model the stream only needs to configured once at the start up.
    Since this driver can work on the n200 or x300, I renamed it to be more
    general.

--- steamed_hams.sh
+++ steamed_hams.sh
@@ -23,26 +23,26 @@ if [ "$2" = "release" ]; then
     start_exphan="sleep 0.001s; python3 -O experiment_handler/experiment_handler.py "$1" ; bash;"
     start_radctrl="sleep 0.001s; python3 -O radar_control/radar_control.py; bash;"
     start_datawrite="sleep 0.001s;python3 -O data_write/data_write.py --file-type=hdf5 --enable-pre-bfiq --enable-bfiq; bash;"
-    start_n200driver="sleep 0.001s; source mode "$2"; n200_driver > n200_output.txt; bash"
+    start_usrp_driver="sleep 0.001s; source mode "$2"; usrp_driver > usrp_output.txt; bash"
     start_dsp="sleep 0.001s; source mode "$2"; signal_processing; bash;"
-    start_tids="sleep 0.001s; python3 -O usrp_drivers/n200/set_affinity.py; bash;"
+    start_tids="sleep 0.001s; python3 -O usrp_drivers/set_affinity.py; bash;"
 elif [ "$2" = "python-profiling" ]; then  # uses source mode release for C code.
     start_brian="python3 -O -m cProfile -o testing/python_testing/brian.cprof brian/brian.py; bash"
     start_exphan="sleep 0.001s; python3 -O -m cProfile -o testing/python_testing/experiment_handler.cprof experiment_handler/experiment_handler.py "$1" ; bash;"
     start_radctrl="sleep 0.001s; python3 -O -m cProfile -o testing/python_testing/radar_control.cprof radar_control/radar_control.py; bash;"
     start_datawrite="sleep 0.001s; python3 -O -m cProfile -o testing/python_testing/data_write.cprof data_write/data_write.py; bash;"
-    start_n200driver="sleep 0.001s; source mode release; n200_driver > n200_output.txt ; read -p 'press enter' "
+    start_usrp_driver="sleep 0.001s; source mode release; usrp_driver > usrp_output.txt ; read -p 'press enter' "
     start_dsp="sleep 0.001s; source mode release; signal_processing; bash;"
-    start_tids="sleep 0.001s; python3 -O usrp_drivers/n200/set_affinity.py; bash;"
+    start_tids="sleep 0.001s; python3 -O usrp_drivers/set_affinity.py; bash;"
 elif [ "$2" = "debug" ] || [ "$2" = "engineeringdebug" ]; then
     start_brian="python3 brian/brian.py; bash"
     start_exphan="sleep 0.001s; python3 experiment_handler/experiment_handler.py "$1" ; bash"
     start_radctrl="sleep 0.001s; python3 radar_control/radar_control.py; bash"
 #    start_datawrite="sleep 0.001s; python3 data_write/data_write.py --enable-bfiq --enable-pre-bfiq --enable-tx --enable-raw-rf; bash"
     start_datawrite="sleep 0.001s; python3 data_write/data_write.py --enable-bfiq --enable-pre-bfiq --enable-raw-rf; bash"
-    start_n200driver="sleep 0.001s; source mode "$2" ; gdb -ex start n200_driver 2>n200_output.txt; bash"
+    start_usrp_driver="sleep 0.001s; source mode "$2" ; gdb -ex start usrp_driver 2>usrp_output.txt; bash"
     start_dsp="sleep 0.001s; source mode "$2"; /usr/local/cuda/bin/cuda-gdb -ex start signal_processing; bash"
-    start_tids="sleep 0.001s; python3 usrp_drivers/n200/set_affinity.py; bash"
+    start_tids="sleep 0.001s; python3 usrp_drivers/set_affinity.py; bash"
 else
     echo "Mode '$2' is unknown, exiting without running Borealis"
     exit -1
@@ -53,7 +53,7 @@ sed -i.bak "s#START_BRIAN#$start_brian#; \
             s#START_EXPHAN#$start_exphan#; \
             s#START_RADCTRL#$start_radctrl#; \
             s#START_DATAWRITE#$start_datawrite#; \
-            s#START_N200DRIVER#$start_n200driver#; \
+            s#START_USRP_DRIVER#$start_usrp_driver#; \
             s#START_DSP#$start_dsp#; \
             s#START_TIDS#$start_tids#;" borealisscreenrc
 
