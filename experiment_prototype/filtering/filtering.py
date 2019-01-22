c1f810a5f732bc442791ee544aba90f0073fad0d
mardet987 <marci.detwiller@usask.ca>
2019-01-22 19:09:23 +0000

    DecimationScheme and DecimationStage classes
    These will be used to create or store decimation stage and filtering
    data for the experiment. They can be easily set up as the default
    provided the rates are as per the default design.

--- experiment_prototype/filtering/filtering.py
+++ experiment_prototype/filtering/filtering.py
@@ -1,4 +1,5 @@
 
+import os
 import sys
 import math
 from scipy import signal
@@ -9,16 +10,19 @@ sys.path.append(BOREALISPATH)
 
 from utils.experiment_options.experimentoptions import ExperimentOptions
 from experiment_prototype.experiment_exception import ExperimentException
+from functools import reduce
 
-class Filter(object):
+class DecimationStage(object):
 
     def __init__(self, stage, input_rate, dm_rate, filter_taps):
         """
-
-        :param stage:
-        :param input_rate:
-        :param dm_rate:
-        :param filter_taps:
+        Create a decimation stage with given decimation rate, input sample rate, and filter taps.
+        :param stage: the index of this filter/decimate stage to all stages (beginning with stage 0)
+        :param input_rate: the input sampling rate, in Hz.
+        :param dm_rate: the decimation rate. Must be an integer.
+        :param filter_taps: a list of filter taps (numeric) to be convolved with the data before the
+        decimation is done.
+        :raises ExperimentException: if types are not correct for signal processing module to use
         """
         self.stage = stage
         self.input_rate = input_rate
@@ -26,39 +30,114 @@ class Filter(object):
             raise ExperimentException('Decimation Rate is not an Integer')
         self.output_rate = input_rate/dm_rate
         self.dm_rate = dm_rate
+        if not isinstance(filter_taps, list):
+            errmsg = 'Filter taps {} must be a list in decimation stage {}'.format(filter_taps,
+                                                                                   stage)
+            raise ExperimentException(errmsg)
+        for x in filter_taps:
+            if not isinstance(x, (int, float, complex)):
+                errmsg = 'Filter tap {} is not numeric in decimation stage {}'.format(x, stage)
+                raise ExperimentException(errmsg)
         self.filter_taps = filter_taps
 
 
-class FilteringScheme(object):
+class DecimationScheme(object):
     """
-    Class for DSP filtering scheme.
+    Class for DSP filtering and decimation scheme.
     """
 
-    options = ExperimentOptions()
+    def __init__(self, rxrate, output_sample_rate, stages=None):
+        """
+        Set up the decimation scheme for the experiment.
+        :param rxrate: sampling rate of USRP, in Hz.
+        :param output_sample_rate: desired output rate of the data, to decimate to, in Hz.
+        :param stages: a list of DecimationStages, or None, if they will be set up as default here.
+        """
+        options = ExperimentOptions()
+        self.num_stages = options.number_of_filtering_stages
+        self.rxrate = rxrate
+        self.output_sample_rate = output_sample_rate
 
-    def __init__(self, filters=None):
-        self.input_rate = []
-        self.dm_rates = []
-        self.output_rates = []
-        self.filter_gains = []
-        if filters is None:
-            # create the default filters, using remez.
+        if stages is None:  # create the default filters, using remez.
+            # Currently only creating default filters if sampling rate and output rate are set
+            # up as per original design.
+            if rxrate != 5.0e6 or round(output_sample_rate, 0) != 3.333e3:
+                errmsg = 'Default filters not defined for rxrate {} and output rate {}'.format(
+                    rxrate, output_sample_rate)
+                raise ExperimentException(errmsg)
 
+            # set up defaults as per design.
+            self.dm_rates = [5, 10, 30, 1]
+            total_rates = [reduce((lambda a, b: a * b), decimation_list) for decimation_list in
+                           [self.dm_rates[:x] for x in range(1, len(self.dm_rates) + 1)]]
+            self.output_rates = [rxrate / x for x in total_rates]
+            input_rates = [rxrate]
+            input_rates.extend(self.output_rates[:-1])
+            self.input_rates = input_rates
+            self.filter_scaling_factors = [1.0, 1.0, 12.0, 1.0]
 
-        self.filters = []
+            # defaults for remez filters for first three stages per design.
+            filter_transition_widths = [500.0e3, 50.0e3, 0.833e3]  # Hz
+            filter_cutoffs = [1.0e6, 100.0e3, 3.333e3]  # Hz
 
+            stages = []
+            for stage_num in range(0, self.num_stages):
+                dm_rate = self.dm_rates[stage_num]
+                input_rate = self.input_rates[stage_num]
+                filter_scaling_factor = self.filter_scaling_factors[stage_num]
 
-    "first_stage_sample_rate" : "1.0e6",
-    "second_stage_sample_rate" : "100.0e3",
-    "first_stage_filter_cutoff" : "1.0e6",
-    "first_stage_filter_transition" : "500.0e3",
-    "second_stage_filter_cutoff" : "100.0e3",
-    "second_stage_filter_transition" : "50.0e3",
-    "third_stage_filter_cutoff" : "3.333e3",
-    "third_stage_filter_transition" : "0.833e3",
-    "first_stage_scaling_factor" : "1.0",
-    "second_stage_scaling_factor" : "1.0",
-    "third_stage_scaling_factor" : "12.0",
+                if stage_num in range(0, 3):
+                    transition_width = filter_transition_widths[stage_num]
+                    cutoff = filter_cutoffs[stage_num]
+                    unity_gain_filter_taps = create_remez_filter(input_rate, cutoff,
+                                                                 transition_width)
+                else:  # append last stage - default is decimation = 1, filter = [1]
+                    unity_gain_filter_taps = [1.0]
+
+                filter_taps = [filter_scaling_factor * i for i in unity_gain_filter_taps]
+
+                dec_stage = DecimationStage(stage_num, input_rate, dm_rate, filter_taps)
+                stages.append(dec_stage)
+            self.stages = stages
+        else:
+            # check that number of stages is correct
+            if len(stages) != self.num_stages:
+                errmsg = 'Decimation stages provided do not meet the required number of stages ' \
+                         '{}'.format(self.num_stages)
+                raise ExperimentException(errmsg)
+            self.dm_rates = []
+            self.output_rates = []
+            self.input_rates = []
+            self.filter_scaling_factors = []
+
+            for dec_stage in stages:
+                self.dm_rates.append(dec_stage.dm_rate)
+                self.output_rates.append(dec_stage.output_rate)
+                self.input_rates.append(dec_stage.input_rate)
+                filter_scaling_factor = reduce((lambda a, b: a + b), dec_stage.filter_taps)
+                self.filter_scaling_factors.append(filter_scaling_factor)
+
+            # check rates are appropriate given rxrate and output_sample_rate, and
+            # check sequentiality of stages, ie output rate transfers to input rate of next stage.
+            if self.input_rates[0] != self.rxrate:
+                errmsg = 'Decimation stage 0 does not have input rate {} equal to USRP sampling ' \
+                         'rate {}'.format(self.input_rates[0], self.rxrate)
+                raise ExperimentException(errmsg)
+
+            for stage_num in range(0, self.num_stages -1):
+                if self.output_rates[stage_num] != self.input_rates[stage_num + 1]:
+                    errmsg = 'Decimation stage {} output rate {} does not equal next stage {} ' \
+                             'input rate {}'.format(stage_num, self.output_rates[stage_num],
+                                                    stage_num + 1, self.input_rates[stage_num + 1])
+                    raise ExperimentException(errmsg)
+
+            if self.output_rates[-1] != self.output_sample_rate:
+                errmsg = 'Last decimation stage {} does not have output rate {} equal to ' \
+                         'requested output data rate {}'.format(self.num_stages - 1,
+                                                                self.output_rates[-1],
+                                                                self.output_sample_rate)
+                raise ExperimentException(errmsg)
+
 
 def calculate_num_filter_taps(sampling_freq, trans_width, k):
     """
@@ -66,16 +145,20 @@ def calculate_num_filter_taps(sampling_freq, trans_width, k):
     transition width desired, and a k value which impacts filter performance (3 typical).
     :param sampling_freq: sampling rate, Hz.
     :param trans_width: transition bandwidth between cutoff of passband and stopband (Hz)
-    :param k: value increasing filter performance with increasing value.
+    :param k: value increasing filter performance with increasing value. Integer.
     :return: num_taps, number of taps for the FIR filter.
     """
-    num_taps = k * (sampling_freq /trans_width)
+    num_taps = int(k * (sampling_freq /trans_width))
     return num_taps
+
 
 def create_remez_filter(sampling_freq, cutoff_freq, trans_width):
     """
     Creates the default filter for the experiment, if the filter taps are not provided.
-    :return:
+    :param sampling_freq: sampling rate, Hz.
+    :param cutoff_freq: cutoff frequency, or edge of passband, Hz.
+    :param trans_width: transition bandwidth between cutoff of passband and stopband (Hz)
+    :return: lowpass filter taps created using the remez method.
     """
 
     num_taps = calculate_num_filter_taps(sampling_freq, trans_width, 3)
@@ -84,13 +167,3 @@ def create_remez_filter(sampling_freq, cutoff_freq, trans_width):
                                     0.5 * sampling_freq], [1, 0], Hz=sampling_freq)
 
     return lpass
-
-def determine_decimation_rates(rxrate, output_rx_rate):
-    """
-    Comes up with decimation rates for four stages, if not provided.
-    :return:
-    """
-    overall_rate = rxrate /output_rx_rate
-    error = overall_rate - int(overall_rate)
-    total_dm_rate = int(overall_rate)
-    return total_dm_rate
