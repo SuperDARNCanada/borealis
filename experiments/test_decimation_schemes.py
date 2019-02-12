32b74fe44f340987342fc4a94baf8c920c609a23
mardet987 <marci.detwiller@usask.ca>
2019-02-12 19:02:32 +0000

    Test scheme 7

--- experiments/test_decimation_schemes.py
+++ experiments/test_decimation_schemes.py
@@ -111,7 +111,7 @@ def create_test_scheme_5():
 	return (DecimationScheme(5.0e6, 10.0e3/3, stages=all_stages))
 
 
-def create_test_scheme_6():
+def create_test_scheme_6(): # tested Feb 12 1800 - looks ~ 10 dB SNR? 
 	"""
 	Create four stages of FIR filters and a decimation scheme. Returns a decimation scheme of type DecimationScheme. 
 	:return DecimationScheme: a decimation scheme for use in experiment.
@@ -130,6 +130,27 @@ def create_test_scheme_6():
 		all_stages.append(DecimationStage(stage, rates[stage], dm_rates[stage], filter_taps))
 
 	all_stages.append(DecimationStage(3, rates[3], dm_rates[3], [1.0]))
+	return (DecimationScheme(5.0e6, 10.0e3/3, stages=all_stages))
+
+
+def create_test_scheme_7(): 
+	"""
+	Create four stages of FIR filters and a decimation scheme. Returns a decimation scheme of type DecimationScheme. 
+	:return DecimationScheme: a decimation scheme for use in experiment.
+	"""
+
+	rates = [5.0e6, 500.0e3/3, 250.0e3/3, 50.0e3/3]
+	dm_rates = [30, 2, 5, 5]
+	transition_widths = [130.0e3, 50.0e3, 18.0e3, 1.2e3]
+	cutoffs = [20.0e3, 2.0e3, 1.0e3, 1.0e3] # bandwidth is double this
+	ripple_dbs = [100.0, 100.0, 40.0, 10.0]
+
+	all_stages = []
+
+	for stage in range(0,4):
+		filter_taps = list(create_firwin_filter_by_attenuation(rates[stage], transition_widths[stage], cutoffs[stage], ripple_dbs[stage]))
+		all_stages.append(DecimationStage(stage, rates[stage], dm_rates[stage], filter_taps))
+
 	return (DecimationScheme(5.0e6, 10.0e3/3, stages=all_stages))
 
 
