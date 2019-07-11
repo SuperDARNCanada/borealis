2d6cd9422ccda5935fa87bbcc115962727537b00
LiamAG <lag649@mail.usask.ca>
2019-07-11 15:16:17 -0600

    Writes output of correlations to file

--- rx_signal_processing/reprocessing.py
+++ rx_signal_processing/reprocessing.py
@@ -1,6 +1,8 @@
 import numpy as np
 import deepdish as dd
 import sys
+import subprocess as sp
+import os
 
 
 def correlate_samples(ts_dict):
@@ -14,16 +16,15 @@ def correlate_samples(ts_dict):
 	num_sequences = ts_dict["data_dimensions"][1]
 	num_beams = ts_dict["data_dimensions"][2]
 	num_samples = ts_dict["data_dimensions"][3]
+	dims = ts_dict["data_dimensions"]
 
 	lags = ts_dict["lags"]
 	num_lags = np.shape(ts_dict["lags"])[0]
 	num_ranges = ts_dict["num_ranges"]
 	num_slices = ts_dict["num_slices"]
 	
-	data_mat = np.reshape(data_buff, (num_ant_arrays,
-	 									num_sequences, 
-	 									num_beams, 
-	 									num_samples,))
+	data_mat = data_buff.reshape(dims)
+
 	# Get data from each antenna array
 	main_data = data_mat[0][:][:][:]
 	intf_data = data_mat[1][:][:][:]
@@ -54,7 +55,7 @@ def correlate_samples(ts_dict):
 			cross_corrs[seq, beam] = np.outer(main_samps, intf_samps.conjugate())
 
 			beam_offset = num_beams * num_ranges * num_lags
-			first_range_offset = ts_dict["first_range"] // ts_dict["range_sep"]
+			first_range_offset = int(ts_dict["first_range"] / ts_dict["range_sep"])
 
 			# Select out the lags for each range gate
 			main_small = intf_small = cross_small = np.zeros((num_ranges, num_lags,), 
@@ -91,9 +92,54 @@ def correlate_samples(ts_dict):
 if __name__ == "__main__":
 	bfiq_filepath = sys.argv[1]
 	acfs_filepath = sys.argv[2]
+	test_acf = "data/test_acf.hdf5"
+	# try:
+	# 	fd = os.open(test_acf, os.O_CREAT | os.O_EXCL)
+	# 	os.close(fd)
+	# except FileExistsError:
+	# 	pass
+	temp_file= "data/temp_acf.hdf5"
 
-	bfiq = dd.io.load(bfiq_filepath, group="/1560967198922")
-	acfs = dd.io.load(acfs_filepath, group="/1560967198922")
+	# bfiq = dd.io.load(bfiq_filepath, group="/1560967198922")
+	# acfs = dd.io.load(acfs_filepath, group="/1560967198922")
+	# my_main, my_intf, my_xcfs = correlate_samples(bfiq)
 
-	my_main, my_intf, my_xcfs = correlate_samples(bfiq)
+	bfiq = dd.io.load(bfiq_filepath)
+	acfs = dict()
 
+	shared_fields = ['beam_azms', 'beam_nums', 'blanked_samples', 'lags', 'noise_at_freq',
+	 					'pulses', 'sqn_timestamps', 'borealis_git_hash', 
+	 					'data_normalization_factor', 'experiment_comment', 
+	 					'experiment_id', 'experiment_name', 'first_range', 
+	 					'first_range_rtt', 'freq', 'int_time', 'intf_antenna_count', 
+	 					'main_antenna_count', 'num_sequences', 'num_slices', 'range_sep', 
+	 					'rx_sample_rate', 'samples_data_type', 'scan_start_marker', 
+	 					'slice_comment', 'station', 'tau_spacing', 'timestamp_of_write', 'tx_pulse_len']
+
+	for k in bfiq:
+		acfs[k] = dict()
+		# write common dictionary fields first
+		for f in shared_fields:
+			acfs[k][f] = bfiq[k][f]
+
+		# Perform correlations and write to dictionary
+		main_acfs, intf_acfs, xcfs = correlate_samples(bfiq[k])
+
+		acfs[k]["correlation_dimensions"] = np.array(main_acfs.shape)
+		acfs[k]["correlation_descriptors"] = np.array(['num_beams', 'num_ranges', 'num_lags'])
+
+		acfs[k]["main_acfs"] = main_acfs.flatten()
+		acfs[k]["intf_acfs"] = intf_acfs.flatten()
+		acfs[k]["xcfs"] = xcfs.flatten()
+
+		ts_dd = {}
+		ts_dd[k] = acfs[k]
+
+		dd.io.save(temp_file, ts_dd, compression=None)
+		cmd = 'h5copy -i {newfile} -o {fullfile} -s {dtstr} -d {dtstr}'
+		cmd = cmd.format(newfile=temp_file, fullfile=test_acf, dtstr=k)
+
+		sp.call(cmd.split())
+		os.remove(temp_file)
+
+		print("Done", k)
--- rx_signal_processing/test_reprocessing.py
+++ rx_signal_processing/test_reprocessing.py
@@ -3,8 +3,8 @@ import deepdish as dd
 from reprocessing import correlate_samples
 
 def test_correlate():
-	acfs = dd.io.load("../data/20190619.1600.02.sas.0.rawacf.hdf5", group="/1560967198922")
-	bfiq = dd.io.load("../data/20190619.1600.02.sas.0.bfiq.hdf5", group="/1560967198922")
+	acfs = dd.io.load("data/20190619.1600.02.sas.0.rawacf.hdf5", group="/1560967198922")
+	bfiq = dd.io.load("data/20190619.1600.02.sas.0.bfiq.hdf5", group="/1560967198922")
 
 	my_main, my_intf, my_xcfs = correlate_samples(bfiq)
 
@@ -15,6 +15,8 @@ def test_correlate():
 	main_acfs = np.reshape(acfs["main_acfs"], (num_beams, num_ranges, num_lags))
 	intf_acfs = np.reshape(acfs["intf_acfs"], (num_beams, num_ranges, num_lags))
 	xcfs_acfs = np.reshape(acfs["xcfs"], (num_beams, num_ranges, num_lags))
+
+	print(my_main / main_acfs)
 
 	np.testing.assert_array_equal(my_main, main_acfs)
 	np.testing.assert_array_equal(my_intf, intf_acfs)
