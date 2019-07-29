531b095597668db97f1fb94b6fe52775548f385a
LiamAG <lag649@mail.usask.ca>
2019-07-29 09:38:26 -0600

    Method for finding fields shared between records
    Wrote a function that finds the fields within a data file that are
    common and uncommon between timestamped records. These fields are
    returned as lists. Also wrote code in the rawacf method for writing
    these shared fields to the data dictionary.

--- utils/post_processing/restructure.py
+++ utils/post_processing/restructure.py
@@ -1,4 +1,4 @@
-import deepdish as deep
+import deepdish as dd
 import numpy as np
 
 def restructure_data(data_path):
@@ -9,22 +9,78 @@ def restructure_data(data_path):
 	Fields from the original record that do not change between records will be stored as fields in one metadata record within
 	the file. Other fields will contain the data arrays and other metadata that does change record to record.
 	"""
-	def restructure_pre_bfiq():
+	def find_shared(data_record):
+		"""
+		Finds fields in an hdf5 file that do not change
+		between records
+		"""
+		shared = list()
+		unshared = list()
+		start = True
+
+		for k in data:
+			if start:
+				data_sub = data_sub[k]
+				start = False
+			else:
+				print("checking", k)
+				for f in data[k]:
+					if type(data[k][f]) is np.ndarray:
+						if np.array_equal(data[k][f], data_sub[f]):
+							if f not in shared:
+								shared.append(f)
+						else:
+							if f in shared:
+								shared.remove(f)
+					else:
+						if data[k][f] == data_sub[f]:
+							if f not in shared:
+								shared.append(f)
+						else:
+							if f in shared:
+								shared.remove(f)
+		for f in data_sub:
+			if f not in shared:
+				unshared.append(f)
+		return shared
+
+	def restructure_pre_bfiq(data_record):
 		"""
 		Restructuring method for pre bfiq data
+		args:
+			data_record		a record loaded from an hdf5
+							data file
 		"""
 
-	def restructure_bfiq():
+	def restructure_bfiq(data_record):
 		"""
 		Restructuring method for bfiq data
+		args:
+			data_record		a record loaded from an hdf5
+							data file
 		"""
 
-	def restructure_rawacf():
+	def restructure_rawacf(data_record):
 		"""
 		Restructuring method for rawacf data
+		args:
+			data_record		a record loaded from an hdf5
+							data file
 		"""
+		data_dict = dict()
+
+		# write shared fields to dictionary
+		shared = find_shared(data_record)
+		k = list(data_record.keys())[0]
+		for f in shared:
+			data_dict[f] = data[k][f]
+
+
+
 
 	suffix = data_path.split('.')[-2]
+
+	data = dd.io.load()
 
 	if suffix == 'output_ptrs_iq':
 		restructure_pre_bfiq()
