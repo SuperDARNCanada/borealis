62c118c2dc344ae7c63d2d717c9a0f451f26b166
LiamAG <lag649@mail.usask.ca>
2019-07-31 13:13:59 -0600

    Started building out logic for antenna iq back conversion

--- utils/post_processing/backconvert_borealis.py
+++ utils/post_processing/backconvert_borealis.py
@@ -1,5 +1,17 @@
+# Copyright 2019 SuperDARN Canada, University of Saskatchewan
+# Author: Liam Graham
+
+#
+# backconvert_borealis.py
+# 2019-07-30
+# Command line tool for converting restructured and compressed
+# Borealis files back to their initial site format
+
 import deepdish as dd
 import numpy as np
+import os
+import sys
+import subprocess as sp
 
 def backconvert(data_path):
 	"""
@@ -16,6 +28,24 @@ def backconvert(data_path):
 		Args:
 			data_record (dict): An opened antennas_iq hdf5 file
 		"""
+		temp = 'temp.hdf5'
+		num_records = len(data_record["int_time"])
+		keys = np.zeros(num_records)
+		ts_dict = dict()
+		# get keys from first sequence timestamps
+		for rec, seq_ts in enumerate(data_record["sqn_timestamps"]):
+			keys[rec] = int(seq_ts[0] * 1000)
+		for rec, k in enumerate(keys):
+			for f in data_record:
+				if type(data_record[f]) is np.ndarray:
+					if not (np.shape(data_record[f])[0] == num_records):
+						ts_dict[f] = data_record[f]
+					else:
+						pass
+						# FIGURE THIS BIT OUT
+				else:
+					ts_dict[f] = data_record[f]
+
 
 	def backconvert_bfiq(data_record):
 		"""
@@ -38,6 +68,14 @@ def backconvert(data_path):
 	print("Restructuring", data_path, "...")
 
 	data = dd.io.load(data_path)
+	converted = data_path + '.site'
+
+	# touch output file
+	try:
+		fd = os.open(converted, os.O_CREAT | os.O_EXCL)
+		os.close(fd)
+	except FileExistsError:
+		pass
 
 	if (suffix == 'output_ptrs_iq') or (suffix == 'antennas_iq'):
 		print("Loaded a pre bfiq file...")
