b2fb88727a79356fd23787260cad3045781f4967
radar <superdarn@gmail.com>
2019-08-02 14:45:59 -0600

    Fixed a couple of mistakes

--- start_radar.sh
+++ start_radar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pkill -9 -f remote_server.py
-python3 $BOREALISPATH/scheduler/remote_server.py --scd-dir=/home/radar/borealis_schedules/borealis_schedules --emails-filepath=/home/radar/borealis_schedules/borealis_schedules/emails.txt 
+nohup python3 $BOREALISPATH/scheduler/remote_server.py --scd-dir=/home/radar/borealis_schedules/borealis_schedules --emails-filepath=/home/radar/borealis_schedules/borealis_schedules/emails.txt >/dev/null &
