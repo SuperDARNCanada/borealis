f180137c65de06ed71f236caddf3ac0b7e651000
mardet987 <marci.detwiller@usask.ca>
2019-08-02 16:10:30 -0600

    Improved borealis_fixer script for batching
    Currently set to update April files as necessary to be able to
    postprocess into rawacfs.

--- utils/post_processing/borealis_fixer.py
+++ utils/post_processing/borealis_fixer.py
@@ -1,3 +1,7 @@
+# Copyright SuperDARN Canada, University of Saskatchewan
+# Author: Marci Detwiller
+
+import argparse
 import sys
 import os
 import copy
@@ -8,10 +12,34 @@ import tables
 warnings.filterwarnings('ignore', category=tables.NaturalNameWarning)
 import deepdish as dd
 
+def usage_msg():
+    """
+    Return the usage message for this process.
+     
+    This is used if a -h flag or invalid arguments are provided.
+    """
 
-def main():
-    filename = sys.argv[1]
-    fixed_data_dir = sys.argv[2]
+    usage_message = """ borealis_fixer.py [-h] filename fixed_dat_dir
+    
+    **** NOT TO BE USED IN PRODUCTION ****
+    **** USE WITH CAUTION ****
+
+    Modify a borealis file with updated data fields. Modify the script where
+    indicated to update the file. Used in commissioning phase of Borealis when 
+    data fields were not finalized."""
+
+    return usage_message
+
+
+def script_parser():
+    parser = argparse.ArgumentParser(usage=usage_msg())
+    parser.add_argument("filename", help="Path to the file that you wish to modify")
+    parser.add_argument("fixed_data_dir", help="Path to place the updated file in.")
+
+    return parser
+
+
+def update_file(filename, fixed_data_dir):
 
     recs = dd.io.load(filename)
     sorted_keys = sorted(list(recs.keys()))
@@ -41,8 +69,17 @@ def main():
 
         # APPLY CHANGE HERE
         # recs[group_name]['data_dimensions'][0] = 2
-        recs[group_name]['main_acfs'] = recs[group_name]['main_acfs'] * -1 
-        recs[group_name]['xcfs'] = recs[group_name]['xcfs'] * -1
+        recs[group_name]['noise_at_freq'] = np.array([0.0] * int(recs[group_name]['num_sequences']), dtype=np.float64)
+        recs[group_name]['data_normalization_factor'] = np.float64(9999999.999999996)
+        recs[group_name]['experiment_comment'] = recs[group_name]['comment']
+        del recs[group_name]['comment']
+        recs[group_name]['slice_comment'] = np.unicode_('')
+        recs[group_name]['experiment_name'] = recs[group_name]['experiment_string']
+        del recs[group_name]['experiment_string']
+        recs[group_name]['num_slices'] = np.int64(1)
+        recs[group_name]['range_sep'] = np.float32(44.96887) 
+        recs[group_name]['num_ranges'] = np.uint32(75)
+        # recs[group_name]['xcfs'] = recs[group_name]['xcfs'] * -1
 
         write_dict = {}
         write_dict[group_name] = convert_to_numpy(recs[group_name])
@@ -58,4 +95,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = script_parser()
+    args = parser.parse_args()
+
+    update_file(args.filename, args.fixed_data_dir)
