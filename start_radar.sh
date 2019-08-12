156ce29528d19448f801ca00ad8783f73068812f
radar <superdarn@gmail.com>
2019-08-12 22:18:18 +0000

    Fixed path to get env variables

--- start_radar.sh
+++ start_radar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-pkill -9 -f remote_server.py
-nohup python3 $BOREALISPATH/scheduler/remote_server.py --scd-dir=/home/radar/borealis_schedules/borealis_schedules --emails-filepath=/home/radar/borealis_schedules/borealis_schedules/emails.txt >/dev/null &
+/usr/bin/pkill -9 -f remote_server.py
+source /home/radar/.profile
+/usr/bin/nohup /usr/bin/python3 $BOREALISPATH/scheduler/remote_server.py --scd-dir=/home/radar/borealis_schedules/borealis_schedules --emails-filepath=/home/radar/borealis_schedules/borealis_schedules/emails.txt >/home/radar/logs/scd.out
