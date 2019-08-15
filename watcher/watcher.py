5ddc44d40b18b3ca35230b5540bef67259065e8a
LiamAG <lag649@mail.usask.ca>
2019-08-15 16:01:26 -0600

    Power calculation method
    Added a method for calculating the lag 0 power for each antenna and
    sequence in an antenna iq file. Method returns the power as an array of
    shape (num_antennas, num_sequences, 70).

--- watcher/watcher.py
+++ watcher/watcher.py
@@ -8,15 +8,46 @@
 # Monitoring process to flag any problems related to rx/tx power
 
 
-import inotify
+import inotify as i
+import deepdish as dd
+import numpy as np
+
+
+def get_lag0_pwr(iq_record):
+	"""
+	Gets lag0 power for each antenna and sequence in a record of 
+	antenna iq data.
+	Args:
+		iq_record: A single antenna iq data record loaded from file.
+	"""
+	# Setup and memory allocation
+	dims = iq_record["data_dimensions"]
+	voltage_samples = iq_record["data"].reshape(dims)
+	num_antennas, num_sequences, num_samps = dims
+	power_array = np.zeros((num_antennas, num_sequences, 70))
+	# get power from voltage samples
+	for antenna in range(num_antennas):
+		for sequence in range(num_sequences):
+			power = (voltage_samples.real**2 + voltage_samples.imag**2)[antenna, sequence, 0:69]
+			power_db = 10 * np.log(power)
+			# set appropriate section of power_array
+			power_array[antenna, sequence, :] = power_db
+
+	return power_array
 
 
 def check_antennas_iq_file_power(iq_file):
 	"""
-	Checks that the power between antennas is reasonably close for each range in a record.
-	If it is not, alert the squad.
+	Checks that the power between antennas is reasonably close for each 
+	range in a record. If it is not, alert the squad.
 	Args:
-		iq_file:	The antenna iq file being checked.
+		iq_file:	The path to the antenna iq file being checked.
 	"""
 
+	ant_iq = dd.io.load(iq_file)
+	antenna_keys = list(ant_iq.keys())
+	first_rec = ant_iq[antenna_keys[0]]
+	last_rec = ant_iq[antenna_keys[-1]]
 
+	first_pwr = get_lag0_pwr(first_rec)
+	last_pwr = get_lag0_pwr(last_rec)
