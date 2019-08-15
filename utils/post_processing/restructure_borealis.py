d4c35ecb53cdf49ebd0fc869e7b9fa0a412611c9
LiamAG <lag649@mail.usask.ca>
2019-08-15 14:48:32 -0600

    PR changes to restructure.py
    One method for restructuring both iq filetypes
    pep8 stuff
    Simple check for filename format when writing the restructured data
    Renamed a confusing variable
    Use Path.touch() to touch output .site format file

--- utils/post_processing/restructure_borealis.py
+++ utils/post_processing/restructure_borealis.py
@@ -13,6 +13,7 @@ import subprocess as sp
 import datetime
 import warnings
 import tempfile
+from pathlib2 import Path
 
 
 ########################## RESTRUCTURING CODE #################################
@@ -49,20 +50,19 @@ def find_max_sequences(data):
 		max_seqs:	The largest number of sequences found 
 					in one record from the file
 	"""
-	first_key = list(data.keys())[0]
-	max_seqs = data[first_key]["num_sequences"]
+	max_seqs = 0
 	for k in data:
 		if max_seqs < data[k]["num_sequences"]:
 			max_seqs = data[k]["num_sequences"]
 	return max_seqs
 
 
-def antennas_iq_site_to_array(data_record):
+def iq_site_to_array(data_record):
 	"""
 	Restructuring method for pre bfiq data
 	args:
-		data_record		a timestamped record loaded from an
-		 				hdf5 Borealis pre-bfiq data file
+		data_record		a timestamped record loaded from an hdf5
+						Borealis antenna iq or bfiq data file
 		 Returns:
 		 	data_dict:	A dictionary containing the data from data_record
 		 				reformatted to be stored entirely as arrays, or as
@@ -72,19 +72,38 @@ def antennas_iq_site_to_array(data_record):
 	num_records = len(data_record)
 
 	# write shared fields to dictionary
-	k = list(data_record.keys())[0]
-	for field in shared_antiq:
-		data_dict[field] = data_record[k][field]
+	first_key = list(data_record.keys())[0]
+
 
 	# find maximum number of sequences
 	max_seqs = find_max_sequences(data_record)
-	dims = data_record[k]["data_dimensions"]
-	num_antennas = dims[0]
-	num_samps = dims[2]
+	dims = data_record[first_key]["data_dimensions"]
 
-	data_buffer_offset = num_antennas * num_samps * max_seqs
-	data_buffer = np.zeros(num_records * data_buffer_offset, dtype=np.complex64)
-	data_shape = (num_records, num_antennas, max_seqs, num_samps)
+	first_dim = data_record[first_key]["data_descriptors"][0]
+	if first_dim == 'num_antennas':  # antenna iq file
+		for field in shared_antiq:
+			data_dict[field] = data_record[first_key][field]
+
+		num_antennas = dims[0]
+		num_samps = dims[2]
+
+		data_buffer_offset = num_antennas * num_samps * max_seqs
+		data_buffer = np.zeros(num_records * data_buffer_offset, dtype=np.complex64)
+		data_shape = (num_records, num_antennas, max_seqs, num_samps)
+
+	elif first_dim == 'num_antenna_arrays':  # bfiq file
+		for field in shared_bfiq:
+			data_dict[field] = data_record[first_key][field]
+		num_arrays = dims[0]
+		num_beams = dims[2]
+		num_samps = dims[3]
+
+		data_buffer_offset = num_arrays * num_beams * num_samps * max_seqs
+		data_buffer = np.zeros(num_records * data_buffer_offset, dtype=np.complex64)
+		data_shape = (num_records, num_arrays, max_seqs, num_beams, num_samps)
+
+	else:  # Unrecognized
+		raise Exception("Unrecognized filetype based on descriptor check")
 
 	sqn_buffer_offset = max_seqs
 	sqn_ts_buffer = np.zeros(num_records * max_seqs)
@@ -97,7 +116,7 @@ def antennas_iq_site_to_array(data_record):
 	sqn_num_array = np.empty(num_records)
 	int_time_array = np.empty(num_records)
 
-	data_dims_array = np.empty((num_records, len(data_record[k]["data_descriptors"])))
+	data_dims_array = np.empty((num_records, len(data_record[first_key]["data_descriptors"])))
 
 	rec_idx = 0
 	for k in data_record:
@@ -138,87 +157,6 @@ def antennas_iq_site_to_array(data_record):
 
 	return data_dict
 
-def bfiq_site_to_array(data_record):
-	"""
-	Restructuring method for bfiq data
-	Args:
-		data_record		a timestamped record loaded from an
-		 				hdf5 Borealis bfiq data file
-	Returns:
-		 data_dict:		A dictionary containing the data from data_record
-		 				reformatted to be stored entirely as arrays, or as
-		 				one entry if the field does not change between records
-	"""
-	data_dict = dict()
-	num_records = len(data_record)
-
-	# write shared fields to dictionary
-	k = list(data_record.keys())[0]
-	for field in shared_bfiq:
-		data_dict[field] = data_record[k][field]
-
-	# find maximum number of sequences
-	max_seqs = find_max_sequences(data_record)
-	dims = data_record[k]["data_dimensions"]
-	num_arrays = dims[0]
-	num_beams = dims[2]
-	num_samps = dims[3]
-
-	data_buffer_offset = num_arrays * num_beams * num_samps * max_seqs
-	data_buffer = np.zeros(num_records * data_buffer_offset, dtype=np.complex64)
-	data_shape = (num_records, num_arrays, max_seqs, num_beams, num_samps)
-
-	sqn_buffer_offset = max_seqs
-	sqn_ts_buffer = np.zeros(num_records * max_seqs)
-	sqn_shape = (num_records, max_seqs)
-
-	noise_buffer_offset = max_seqs
-	noise_buffer = np.zeros(num_records * max_seqs)
-	noise_shape = (num_records, max_seqs)
-
-	sqn_num_array = np.empty(num_records)
-	int_time_array = np.empty(num_records)
-
-	data_dims_array = np.empty((num_records, len(data_record[k]["data_descriptors"])))
-
-	rec_idx = 0
-	for k in data_record:
-		# handle unshared fields
-		int_time_array[rec_idx] = data_record[k]["int_time"]
-		sqn_num_array[rec_idx] = data_record[k]["num_sequences"]
-		data_dims_array[rec_idx] = data_record[k]["data_dimensions"]
-
-		# insert data into buffer
-		record_buffer = data_record[k]["data"]
-		data_pos = rec_idx * data_buffer_offset
-		data_end = data_pos + len(record_buffer)
-		data_buffer[data_pos:data_end] = record_buffer
-
-		# insert sequence timestamps into buffer
-		rec_sqn_ts = data_record[k]["sqn_timestamps"]
-		sqn_pos = rec_idx * sqn_buffer_offset
-		sqn_end = sqn_pos + data_record[k]["num_sequences"]
-		sqn_ts_buffer[sqn_pos:sqn_end] = rec_sqn_ts
-
-		rec_noise = data_record[k]["noise_at_freq"]
-		noise_pos = rec_idx * noise_buffer_offset
-		noise_end = noise_pos + data_record[k]["num_sequences"]
-		noise_buffer[noise_pos:noise_end] = rec_noise
-
-		rec_idx += 1
-
-	# write leftover metadata and data
-	data_dict["int_time"] = int_time_array
-	data_dict["num_sequences"] = sqn_num_array
-
-	data_dict["data"] = data_buffer.reshape(data_shape)
-	data_dict["sqn_timestamps"] = sqn_ts_buffer.reshape(sqn_shape)
-	data_dict["noise_at_freq"] = noise_buffer.reshape(noise_shape)
-
-	data_dict["data_descriptors"] = np.insert(data_dict["data_descriptors"], 0, "num_records")
-	data_dict["data_dimensions"] = data_dims_array
-
-	return data_dict
 
 def rawacf_site_to_array(data_record):
 	"""
@@ -236,9 +174,9 @@ def rawacf_site_to_array(data_record):
 	max_seqs = find_max_sequences(data_record)
 
 	# write shared fields to dictionary
-	k = list(data_record.keys())[0]
+	first_key = list(data_record.keys())[0]
 	for field in shared_rawacf:
-		data_dict[field] = data_record[k][field]
+		data_dict[field] = data_record[first_key][field]
 
 	# handle unshared data fields
 	dims = data_dict["correlation_dimensions"]
@@ -308,7 +246,10 @@ def write_array_format_data(data_dict, data_path):
 		data_path:		The path to the data file storing data_dict
 	"""
 	print("Compressing...")
-
+	# Quick check of path format
+	filename = data_path.split('/')[-1]
+	if not len(filename.split('.')) == 7:
+		raise Exception("filename does not match standard format")
 	dd.io.save(data_path[:-5], data_dict, compression='zlib')
 
 
@@ -330,11 +271,11 @@ def site_to_array_format(data_path):
 
 	if ('output_ptrs_iq' in data_path) or ('antennas_iq' in data_path):
 		print("Loaded an antenna iq file...")
-		ant_iq = antennas_iq_site_to_array(data)
+		ant_iq = iq_site_to_array(data)
 		write_array_format_data(ant_iq, data_path)
 	elif 'bfiq' in data_path:
 		print("Loaded a bfiq file...")
-		bfiq = bfiq_site_to_array(data)
+		bfiq = iq_site_to_array(data)
 		write_array_format_data(bfiq, data_path)
 	elif 'rawacf' in data_path:
 		print("Loaded a raw acf file")
@@ -453,15 +394,10 @@ def write_site_format_data(ts_dict, data_path):
 	"""
 	temp_file = tempfile.NamedTemporaryFile().name
 	site_format_file = data_path + '.site'
+	Path(site_format_file).touch()
 	for key in ts_dict:
 		time_stamped_dd = {}
 		time_stamped_dd[key] = ts_dict[key]
-		# touch output file
-		try:
-			fd = os.open(site_format_file, os.O_CREAT | os.O_EXCL)
-			os.close(fd)
-		except FileExistsError:
-			pass
 
 		dd.io.save(temp_file, time_stamped_dd, compression=None)
 		cmd = 'h5copy -i {newfile} -o {fullfile} -s {dtstr} -d {dtstr}'
