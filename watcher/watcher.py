3882b6a563a10f30170ebe4d1227ac8b293ddba3
LiamAG <lag649@mail.usask.ca>
2019-08-15 16:49:14 -0600

    Added function to build a giant boolean array
    The array shape is (num_antennas, num_antennas, num_sequences, num_ranges)
    It stores the truth value of whether a point (seq_num, rng_num) is
    withing a threshold between ant_1 and ant_2. So, if the point (0, 5, 6,
    48) is True, then the power of sequence 6, range 48 at antenna 0 is
    within the set threshold of the same power at antenna 5.

--- watcher/watcher.py
+++ watcher/watcher.py
@@ -18,7 +18,11 @@ def get_lag0_pwr(iq_record):
 	Gets lag0 power for each antenna and sequence in a record of 
 	antenna iq data.
 	Args:
-		iq_record: A single antenna iq data record loaded from file.
+		iq_record:	 A single antenna iq data record loaded from file.
+	Returns:
+		power_array: A numpy array of shape (num_antennas, num_sequences, 70)
+					 containing the lag 0 power for each antenna and sequence
+					 in the iq_record
 	"""
 	# Setup and memory allocation
 	dims = iq_record["data_dimensions"]
@@ -36,12 +40,43 @@ def get_lag0_pwr(iq_record):
 	return power_array
 
 
-def check_antennas_iq_file_power(iq_file):
+def build_truth(power_array, threshold):
+	"""
+	Compares each data point (seq, range) for each antenna against every 
+	other antenna and builds an array of booleans of shape
+	(num_antennas, num_antennas, num_sequences, num_ranges)
+	Each point is the truth value of whether that point is within a difference
+	threshold for each antenna. For example, if point (0, 5, 6, 49) is True: 
+	then the power level difference between antennas 0 and 5 for sequence 6
+	range 50 is within the threshold.
+	Args:
+		power_array:	An np.ndarray of powers in dB created by get_lag0_pwr.
+		threshold:		The acceptable difference in power between antennas.
+	Returns:
+		the_truth:		Array of booleans as described above.
+	"""
+	num_antennas, num_sequences, num_ranges = power_array.shape
+	the_truth = np.zeros((num_antennas, num_antennas, num_sequences, num_ranges),
+							dtype=bool)
+	for ant in range(num_antennas):
+		for comp in range(num_antennas):
+			for seq in range(num_sequences):
+				for rng in range(num_ranges):
+					if (power_array[ant, seq, rng] - power_array[comp, seq, rng]) < threshold:
+						the_truth[ant, comp, seq, rng] = True
+					else:
+						the_truth[ant, comp, seq, rng] = False
+
+	return the_truth
+
+
+def check_antennas_iq_file_power(iq_file, threshold):
 	"""
 	Checks that the power between antennas is reasonably close for each 
 	range in a record. If it is not, alert the squad.
 	Args:
-		iq_file:	The path to the antenna iq file being checked.
+		iq_file:		The path to the antenna iq file being checked.
+		threshold:		The acceptable difference in power between antennas.
 	"""
 
 	ant_iq = dd.io.load(iq_file)
@@ -51,3 +86,6 @@ def check_antennas_iq_file_power(iq_file):
 
 	first_pwr = get_lag0_pwr(first_rec)
 	last_pwr = get_lag0_pwr(last_rec)
+
+	first_truth = build_truth(first_pwr, threshold)
+	last_truth = build_truth(last_pwr, threshold)
