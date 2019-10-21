08922ed2d2e7de1ca00fe79ab920021343eedb48
radar <radar@borealis.usask.ca>
2019-10-21 22:18:41 +0000

    Editing path for borealis schedules

--- start_radar.sh
+++ start_radar.sh
@@ -2,4 +2,4 @@
 
 /usr/bin/pkill -9 -f remote_server.py
 source /home/radar/.profile
-/usr/bin/nohup /usr/bin/python3 $BOREALISPATH/scheduler/remote_server.py --scd-dir=/home/radar/borealis_schedules/borealis_schedules --emails-filepath=/home/radar/borealis_schedules/borealis_schedules/emails.txt >/home/radar/logs/scd.out &
+/usr/bin/nohup /usr/bin/python3 $BOREALISPATH/scheduler/remote_server.py --scd-dir=/home/radar/borealis_schedules --emails-filepath=/home/radar/borealis_schedules/emails.txt >/home/radar/logs/scd.out &
