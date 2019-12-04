2b30cabb7d586f91c4f73021dcf8d713c901ee1c
Keith Kotyk <keithkotyk@gmail.com>
2019-12-04 15:51:56 -0600

    fixing typo

--- scheduler/remote_server.py
+++ scheduler/remote_server.py
@@ -481,7 +481,7 @@ def get_relevant_lines(scd_util, time_of_interest):
     while not found:
 
         if not relevant_lines:
-            msg = "Error in schedule: Could not find any relevant_lines. Either no lines exist "
+            msg = "Error in schedule: Could not find any relevant_lines. Either no lines exist " \
             "or an infinite duration line could not be found."
 
             raise ValueError(msg)
@@ -489,8 +489,8 @@ def get_relevant_lines(scd_util, time_of_interest):
         lines_dict = {}
         for line in relevant_lines:
             if line['timestamp'] not in lines_dict:
-                lines_dict['timestamp'] = []
-            lines_dict['timestamp'].append(line)
+                lines_dict[line['timestamp']] = []
+            lines_dict[line['timestamp']].append(line)
 
         for line in lines_dict[list(lines_dict)[0]]:
             if line['duration'] == '-':
@@ -504,7 +504,7 @@ def get_relevant_lines(scd_util, time_of_interest):
 
             new_relevant_lines = scd_util.get_relevant_lines(yyyymmdd, hhmm)
             if not new_relevant_lines:
-                msg = "Error in schedule: Could not find any relevant_lines. Either no lines exist "
+                msg = "Error in schedule: Could not find any relevant_lines. Either no lines exist "\
                 "or an infinite duration line could not be found."
 
                 raise ValueError(msg)
