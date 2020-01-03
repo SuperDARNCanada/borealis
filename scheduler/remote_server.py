2dc546ee095e4b4d38378b13d0b7804e46248772
Keith Kotyk <keithkotyk@gmail.com>
2020-01-03 13:54:12 -0600

    New start up script
    Replaced the .sh file with a .py file with improvements. The python file
    will now generate the borealisscreenrc file at runtime. The script more
    generalizes the formatting of borealis programs and options. This script
    also adds logging outputs from each program.

--- scheduler/remote_server.py
+++ scheduler/remote_server.py
@@ -38,7 +38,7 @@ def format_to_atq(dt, experiment, first_event_flag=False):
         str: Formatted atq str.
     """
 
-    start_cmd = "echo 'screen -d -m -S starter {borealis_path}/steamed_hams.sh {experiment} release'"
+    start_cmd = "echo 'screen -d -m -S starter {borealis_path}/steamed_hams.py {experiment} release'"
     start_cmd = start_cmd.format(borealis_path=os.environ['BOREALISPATH'],experiment=experiment)
     if first_event_flag:
         cmd_str = start_cmd + " | at now + 1 minute"
