7e94410504e4b4b81324ba8e10ab863ecdca5014
radar <radar@invborealis.usask.ca>
2020-04-20 22:50:06 +0000

    fixing some tauscan bugs

--- experiments/tauscan.py
+++ experiments/tauscan.py
@@ -24,32 +24,37 @@ class Tauscan(ExperimentPrototype):
     def __init__(self):
         cpid = 503
 
+        super(Tauscan, self).__init__(cpid)
+
         if scf.IS_FORWARD_RADAR:
             beams_to_use = scf.STD_16_FORWARD_BEAM_ORDER
         else:
             beams_to_use = scf.STD_16_REVERSE_BEAM_ORDER
 
         if scf.opts.site_id in ["sas", "pgr", "cly"]:
-            freq = 13500,
+            freq = 13500
         if scf.opts.site_id in ["rkn"]:
             freq = 10200
         if scf.opts.site_id in ["inv"]:
             freq = 10300
 
+        print(freq)
         slice_1 = {
             "pulse_sequence": [0, 10, 13, 14, 19, 21, 31, 33, 38, 39, 42],
             "tau_spacing": 3000,
             "pulse_len": scf.PULSE_LEN_45KM,
-            "num_ranges": scf.STD_NUM_RANGES,
+            "num_ranges": 100,
             "first_range": scf.STD_FIRST_RANGE,
             "intt": 7000,  # duration of an integration, in ms
             "beam_angle": scf.STD_16_BEAM_ANGLE,
             "beam_order": beams_to_use,
             "scanbound" : [i * 7.0 for i in range(len(beams_to_use))],
-            "txfreq" : 10500, #kHz
+            "txfreq" : freq, #kHz
+            "acf" : True,
+            "xcf" : True,
+            "acfint" : True,
         }
         
-        super(Tauscan, self).__init__(cpid)
 
         self.add_slice(slice_1)
 
