f98930f2d94536ff497320714a929d8775a76ab1
radar <radar@invborealis.usask.ca>
2020-07-03 20:35:19 +0000

    Tee append

--- start_radar.sh
+++ start_radar.sh
@@ -4,13 +4,12 @@
 source $HOME/.profile
 
 NOW=`date +'%Y%m%d %H:%M:%S'`
-echo "${NOW} START: Attempting to start all radar processes." | tee /data/borealis_logs/start_stop.log
 
 /usr/bin/nohup /usr/bin/python3 $BOREALISPATH/scheduler/remote_server.py --scd-dir=/home/radar/borealis_schedules --emails-filepath=/home/radar/borealis_schedules/emails.txt >/home/radar/logs/scd.out 2>&1 &
 
 retVal=$?
 if [[ $retVal -ne 0 ]]; then
-	echo "${NOW} START: Could not start radar." | tee /data/borealis_logs/start_stop.log
+	echo "${NOW} START: Could not start radar." | tee -a /data/borealis_logs/start_stop.log
 else
-	echo "${NOW} START: Radar processes started." | tee /data/borealis_logs/start_stop.log
+	echo "${NOW} START: Radar processes started." | tee -a /data/borealis_logs/start_stop.log
 fi
