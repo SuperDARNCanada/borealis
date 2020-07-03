dcf9dd4cc176d5f74b3e47c4e725ba699e50643a
radar <radar@invborealis.usask.ca>
2020-07-03 20:35:19 +0000

    Tee append

--- stop_radar.sh
+++ stop_radar.sh
@@ -8,13 +8,12 @@ do
 done  
 
 NOW=`date +'%Y%m%d %H:%M:%S'`
-echo "${NOW} STOP: Attempting to stop all radar processes." | tee /data/borealis_logs/start_stop.log
 
-screen -X -S borealis quit | tee /data/borealis_logs/start_stop.log
+screen -X -S borealis quit | tee -a /data/borealis_logs/start_stop.log
 
 retVal=$?
 if [[ $retVal -ne 0 ]]; then
-	echo "${NOW} STOP: Could not stop radar." | tee /data/borealis_logs/start_stop.log
+	echo "${NOW} STOP: Could not stop radar." | tee -a /data/borealis_logs/start_stop.log
 else
-	echo "${NOW} STOP: Radar processes stopped." | tee /data/borealis_logs/start_stop.log
+	echo "${NOW} STOP: Radar processes stopped." | tee -a /data/borealis_logs/start_stop.log
 fi
