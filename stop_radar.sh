1d26c87ab54ccb4f767304d72659dc9c87547b9b
mardet987 <marci.detwiller@usask.ca>
2020-07-03 14:03:36 -0600

    Logging stop_radar and start_radar runs

--- stop_radar.sh
+++ stop_radar.sh
@@ -7,5 +7,14 @@ do
 	atrm $i
 done  
 
-screen -X -S borealis quit
+NOW=`date +'%Y%m%d %H:%M:%S'`
+echo "${NOW} STOP: Attempting to stop all radar processes." | tee /data/borealis_logs/start_stop.log
 
+screen -X -S borealis quit | tee /data/borealis_logs/start_stop.log
+
+retVal=$?
+if [[ $retVal -ne 0 ]]; then
+	echo "${NOW} STOP: Could not stop radar." | tee /data/borealis_logs/start_stop.log
+else
+	echo "${NOW} STOP: Radar processes stopped." | tee /data/borealis_logs/start_stop.log
+fi
