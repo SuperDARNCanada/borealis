cdcb729518f23f1db82ab02215869afb5ed9573e
mardet987 <marci.detwiller@gmail.com>
2020-08-17 15:43:13 -0600

    Remove outdated comment

--- data_write/data_write.py
+++ data_write/data_write.py
@@ -1065,7 +1065,7 @@ class DataWrite(object):
                 parameters['intf_antenna_count'] = np.uint32(len(rx_freq.rx_intf_antennas))
                 parameters['freq'] = np.uint32(rx_freq.rxfreq)
                 parameters[
-                    'rx_center_freq'] = integration_meta.rx_center_freq  # Sorry, we'll convert to US English here
+                    'rx_center_freq'] = integration_meta.rx_center_freq 
                 parameters['samples_data_type'] = "complex float"
                 parameters['pulses'] = np.array(rx_freq.ptab.pulse_position, dtype=np.uint32)
 
