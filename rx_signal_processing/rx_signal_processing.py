7936dfaf490e3657eb9a7e9b7d042ad79db803ee
keith <keith@kotyk.io>
2020-10-01 15:17:18 -0600

    fixing bugs

--- rx_signal_processing/rx_signal_processing.py
+++ rx_signal_processing/rx_signal_processing.py
@@ -7,6 +7,7 @@ import numpy as np
 import posix_ipc as ipc
 import zmq
 import dsp
+import math
 
 borealis_path = os.environ['BOREALISPATH']
 if not borealis_path:
@@ -21,7 +22,7 @@ import rxsamplesmetadata_pb2
 import sigprocpacket_pb2
 
 sys.path.append(borealis_path + '/utils/')
-import signal_processinging_options.signal_processinging_options as spo
+import signal_processing_options.signal_processing_options as spo
 from zmq_borealis_helpers import socket_operations as so
 
 def printing(msg):
@@ -39,41 +40,22 @@ def main():
     sockets = so.create_sockets([sig_options.dsp_to_radctrl_identity,
                                     sig_options.dsp_to_driver_identity,
                                     sig_options.dsp_to_exphan_identity,
-                                    sig_options.dspbegin_to_brian_identity,
-                                    sig_options.dspend_to_brian_identity,
-                                    sig_options.dsp_to_dw_identity])
+                                    sig_options.dsp_to_dw_identity], sig_options.router_address)
 
     dsp_to_radar_control = sockets[0]
     dsp_to_driver = sockets[1]
     dsp_to_experiment_handler = sockets[2]
-    dspbegin_to_brian = sockets[3]
-    dspend_to_brian = sockets[4]
-    dsp_to_dw = sockets[5]
-
-    #poller = zmq.Poller()
-    #poller.register(dsp_to_radar_control, zmq.POLLIN)
-    #poller.register(dsp_to_driver, zmq.POLLIN)
-    #poller.register(dsp_to_experiment_handler, zmq.POLLIN)
+    dsp_to_dw = sockets[3]
 
     ringbuffer = None
-    shm = ipc.SharedMemory(sig_options.ringbuffer_name)
-    mapped_mem = mmap.mmap(shm.fd, shm.size)
 
     total_antennas = sig_options.main_antenna_count + sig_options.intf_antenna_count
 
     dm_rates = []
     dm_scheme_taps = []
-    output_sample_rate = None
-    offset_to_first_rx_sample = None
-    rx_rate = sig_options.rx_rate
 
     first_time = True
     while True:
-
-        # try:
-        #     socks = dict(poller.poll())
-        # except KeyboardInterrupt:
-        #     sys.exit()
 
         reply = so.recv_bytes(dsp_to_radar_control, options.radctrl_to_dsp_identity, printing)
 
@@ -97,13 +79,13 @@ def main():
             detail['first_range'] = chan.first_range
             detail['range_sep'] = chan.range_sep
             detail['tau_spacing'] = chan.tau_spacing
+            detail['num_range_gates'] = chan.num_ranges
 
             lags = []
-            for lag in chan.lag:
+            for lag in chan.lags:
                 lags.append([lag.pulse_1,lag.pulse_2])
 
             detail['lags'] = lags
-
 
             main_beams = []
             intf_beams = []
@@ -120,9 +102,9 @@ def main():
                 main_beams.append(main_beam)
                 intf_beams.append(intf_beam)
 
-            details['num_beams'] = len(main_beams)
+            detail['num_beams'] = len(main_beams)
 
-            slice_details.append(details)
+            slice_details.append(detail)
             main_beam_angles.append(main_beams)
             intf_beam_angles.append(intf_beams)
 
@@ -145,7 +127,7 @@ def main():
 
         message = "Need data to process"
         so.send_data(dsp_to_driver, sig_options.driver_to_dsp_identity, message)
-        reply = so.recv_bytes(dsp_to_driver, sig_options.driver_to_dsp_identity)
+        reply = so.recv_bytes(dsp_to_driver, sig_options.driver_to_dsp_identity, printing)
 
         rx_metadata = rxsamplesmetadata_pb2.RxSamplesMetadata()
         rx_metadata.ParseFromString(reply)
@@ -173,17 +155,20 @@ def main():
 
         offset_to_first_rx_sample = int(sp_packet.offset_to_first_rx_sample * rx_rate)
 
-
         sample_time_diff = rx_metadata.sequence_start_time - rx_metadata.initialization_time
         sample_in_time = (sample_time_diff * rx_rate) + offset_to_first_rx_sample - extra_samples
 
         start_sample = int(math.fmod(sample_in_time, rx_metadata.ringbuffer_size))
 
-        indices = np.arange(start_sample, samples_needed)
+        indices = np.arange(start_sample, start_sample + samples_needed)
 
-        request = so.recv_bytes(dspbegin_to_brian, sig_options.brian_to_dspbegin_identity)
+        gpu_socks = so.create_sockets([sig_options.dspbegin_brian_identity + str(sp_packet.sequence_num),
+                                        sig_options.dspend_brian_identity + str(sp_packet.sequence_num)],
+                                        sig_options.router_address)
+
+        request = so.recv_bytes(gpu_socks[0], sig_options.brian_dspbegin_identity, printing)
         msg = 'Send start ack for sequence num #' + str(sp_packet.sequence_num())
-        so.send_data(dspbegin_to_brian, sig_options.brian_to_dspbegin_identity)
+        so.send_data(gpu_socks[0], sig_options.brian_dspbegin_identity, msg)
 
         main_buffer = ringbuffer[:sig_options.main_antenna_count,:]
         main_sequence_samples = main_buffer.take(indices, axis=1, mode='wrap')
@@ -208,9 +193,9 @@ def main():
                             processed_intf_samples.beamformed_samples, output_sample_rate,
                             slice_details)
 
-        request = so.recv_bytes(dspend_to_brian, sig_options.brian_to_dspend_identity)
+        request = so.recv_bytes(gpu_socks[1], sig_options.brian_dspend_identity, printing)
         msg = 'Send end for sequence num #' + str(sp_packet.sequence_num())
-        so.send_data(dspend_to_brian, sig_options.brian_to_dspend_identity)
+        so.send_data(gpu_socks[1], sig_options.brian_dspend_identity, msg)
 
 
 
