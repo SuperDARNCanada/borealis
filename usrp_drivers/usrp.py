2d0b28988ad21cffa5404c80418d362dfce31a82
RemingtonRohel <rar129@usask.ca>
2021-12-10 14:12:29 -0600

    Cleaned up imports in usrp.py

--- usrp_drivers/usrp.py
+++ usrp_drivers/usrp.py
@@ -6,44 +6,12 @@ Modified: Dec. 9, 2021 by Remington Rohel
 This file was adapted into Python in December 2021 by Remington Rohel, based
 off of the file usrp_driver.cpp written by Keith Kotyk.
 """
-import sys
-import os
-import mmap
 import time
-import threading
 import numpy as np
-import posix_ipc as ipc
-import mmap
-import zmq
-import math
-import copy
 import uhd
 
+import utils.shared_macros.shared_macros as sm
 from utils.driver_options.driveroptions import DriverOptions
-
-try:
-    import cupy as cp
-except:
-    cupy_available = False
-else:
-    cupy_available = True
-
-borealis_path = os.environ['BOREALISPATH']
-if not borealis_path:
-    raise ValueError("BOREALISPATH env variable not set")
-
-if __debug__:
-    sys.path.append(borealis_path + '/build/debug/utils/protobuf')
-else:
-    sys.path.append(borealis_path + '/build/release/utils/protobuf')
-
-import rxsamplesmetadata_pb2
-import sigprocpacket_pb2
-import processeddata_pb2
-
-sys.path.append(borealis_path + '/utils/')
-from zmq_borealis_helpers import socket_operations as so
-import shared_macros.shared_macros as sm
 
 pprint = sm.MODULE_PRINT("n200 driver", "yellow")
 
