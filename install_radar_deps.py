b91a7a8b14f620900ac03fc03adc7002ed8d1961
Remington Rohel <rar129@usask.ca>
2022-06-23 22:54:01 +0000

    Fixed some git links and installation steps.
    * Made more PEP8

--- install_radar_deps.py
+++ install_radar_deps.py
@@ -16,6 +16,7 @@ import os
 import multiprocessing as mp
 import argparse as ap
 
+
 def usage_msg():
     """
     Return the usage message for this process.
@@ -36,6 +37,7 @@ def usage_msg():
     """
     return usage_message
 
+
 def execute_cmd(cmd):
     """
     Execute a shell command and return the output
@@ -48,6 +50,7 @@ def execute_cmd(cmd):
     """
     output = sp.check_output(cmd, shell=True)
     return output.decode('utf-8')
+
 
 def get_distribution():
     """
@@ -69,6 +72,7 @@ def get_distribution():
         distro = os_info[0].strip("NAME=").strip('"')
 
     return distro
+
 
 def install_packages():
     """
@@ -158,12 +162,12 @@ def install_packages():
                 ]
 
     pip = ["deepdish",
-            "posix_ipc",
-            "inotify",
-            "matplotlib",
-            "virtualenv",
-            "protobuf",
-            "zmq"]
+           "posix_ipc",
+           "inotify",
+           "matplotlib",
+           "virtualenv",
+           "protobuf",
+           "zmq"]
 
     if "openSUSE" in DISTRO:
         pck_mgr = 'zypper'
@@ -178,8 +182,8 @@ def install_packages():
         print(install_cmd)
         try:
             execute_cmd(install_cmd)
-        except sp.CalledProcessError as e:
-            print(e)
+        except sp.CalledProcessError as err:
+            print(err)
 
     update_pip = "pip3.9 install --upgrade pip"
     execute_cmd(update_pip)
@@ -194,44 +198,47 @@ def install_protobuf():
     """
 
     proto_cmd = "cd ${IDIR};" \
-    "git clone https://github.com/google/protobuf.git;" \
-    "cd protobuf || exit;" \
-    "./autogen.sh;" \
-    "./configure;" \
-    "make -j${CORES};" \
-    "make -j${CORES} check;" \
-    "make install;" \
-    "ldconfig;"
+                "git clone https://github.com/protocolbuffers/protobuf.git;" \
+                "cd protobuf || exit;" \
+                "./autogen.sh;" \
+                "./configure;" \
+                "make -j${CORES};" \
+                "make -j${CORES} check;" \
+                "make install;" \
+                "ldconfig;"
 
     execute_cmd(proto_cmd)
+
 
 def install_zmq():
     """
     Install ZMQ and C++ bindings.
     """
+    libsodium_cmd = "cd ${IDIR};" \
+                    "wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz;" \
+                    "tar xzf LATEST.tar.gz;" \
+                    "cd libsodium-stable || exit;" \
+                    "./configure;" \
+                    "make && make check;" \
+                    "make install;" \
+                    "ldconfig;"
+    execute_cmd(libsodium_cmd)
 
     zmq_cmd = "cd ${IDIR};" \
-    "git clone git://github.com/jedisct1/libsodium.git;" \
-    "cd libsodium || exit;" \
-    "git checkout stable;" \
-    "./autogen.sh;" \
-    "./configure && make -j${CORES} check;" \
-    "make install;" \
-    "ldconfig;" \
-    "cd ../ || exit;" \
-    "git clone git://github.com/zeromq/libzmq.git;" \
-    "cd libzmq || exit;" \
-    "./autogen.sh;" \
-    "./configure --with-libsodium && make -j${CORES};" \
-    "make install;" \
-    "ldconfig;" \
-    "cd ../ || exit;" \
-    "git clone https://github.com/zeromq/cppzmq.git;" \
-    "cd cppzmq || exit;" \
-    "cp zmq.hpp /usr/local/include/;" \
-    "cp zmq_addon.hpp /usr/local/include;"
+              "git clone https://github.com/zeromq/libzmq.git;" \
+              "cd libzmq || exit;" \
+              "./autogen.sh;" \
+              "./configure --with-libsodium && make -j${CORES};" \
+              "make install;" \
+              "ldconfig;" \
+              "cd ../ || exit;" \
+              "git clone https://github.com/zeromq/cppzmq.git;" \
+              "cd cppzmq || exit;" \
+              "cp zmq.hpp /usr/local/include/;" \
+              "cp zmq_addon.hpp /usr/local/include;"
 
     execute_cmd(zmq_cmd)
+
 
 def install_ntp():
     """
@@ -239,26 +246,25 @@ def install_ntp():
     """
 
     ntp_cmd = "cd ${IDIR};" \
-    "cp -v /usr/include/sys/timepps.h /usr/include/ || exit;" \
-    "wget -N http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/ntp-4.2.8p13.tar.gz;" \
-    "tar xvf ntp-4.2.8p13.tar.gz;" \
-    "cd ntp-4.2.8p13/ || exit;" \
-    "./configure --enable-atom;" \
-    "make -j${CORES};" \
-    "make install;"
+              "cp -v /usr/include/sys/timepps.h /usr/include/ || exit;" \
+              "wget -N http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/ntp-4.2.8p13.tar.gz;" \
+              "tar xvf ntp-4.2.8p13.tar.gz;" \
+              "cd ntp-4.2.8p13/ || exit; " \
+              "./configure --enable-atom;" \
+              "make -j${CORES};" \
+              "make install;"
 
     execute_cmd(ntp_cmd)
+
 
 def install_uhd():
     """
     Install UHD. UHD is particular about which version of boost it uses, so check that.
     """
 
-
     def fix_boost_links():
         import glob
         import pathlib as pl
-        libpath = ""
 
         if "openSUSE" in DISTRO:
             libpath = '/usr/lib64/'
@@ -289,10 +295,11 @@ def install_uhd():
 
         print(files_with_no_ext)
 
-        for (f,n) in zip(files, files_with_no_ext):
+        for (f, n) in zip(files, files_with_no_ext):
             cmd = 'ln -s -f {} {}/{}.so'.format(f, libpath, n)
             execute_cmd(cmd)
 
+        cmd = ""
         if "openSUSE" in DISTRO:
             cmd = 'ln -s -f {libpath}/libboost_python-py3.so {libpath}/libboost_python3.so'.format(libpath=libpath)
         elif "Ubuntu" in DISTRO:
@@ -303,44 +310,48 @@ def install_uhd():
     fix_boost_links()
 
     uhd_cmd = "cd ${IDIR};" \
-    "git clone --recursive git://github.com/EttusResearch/uhd.git;" \
-    "cd uhd || exit;" \
-    "git checkout UHD-4.0;" \
-    "git submodule init;" \
-    "git submodule update;" \
-    "cd host || exit;" \
-    "mkdir build;" \
-    "cd build || exit;" \
-    "cmake -DENABLE_PYTHON3=on -DPYTHON_EXECUTABLE=$(which python3) -DRUNTIME_PYTHON_EXECUTABLE=$(which python3) -DENABLE_PYTHON_API=ON -DENABLE_DPDK=OFF ../;" \
-    "make -j${CORES};" \
-    "make -j${CORES} test;" \
-    "make install;" \
-    "ldconfig;"
+              "git clone --recursive https://github.com/EttusResearch/uhd.git;" \
+              "cd uhd || exit;" \
+              "git checkout UHD-4.0;" \
+              "git submodule init;" \
+              "git submodule update;" \
+              "cd host || exit;" \
+              "mkdir build;" \
+              "cd build || exit;" \
+              "cmake -DENABLE_PYTHON3=on -DPYTHON_EXECUTABLE=$(which python3) " \
+              "-DRUNTIME_PYTHON_EXECUTABLE=$(which python3) -DENABLE_PYTHON_API=ON -DENABLE_DPDK=OFF ../;" \
+              "make -j${CORES};" \
+              "make -j${CORES} test;" \
+              "make install;" \
+              "ldconfig;"
 
     execute_cmd(uhd_cmd)
+
 
 def install_cuda():
     """
     Install CUDA.
     """
+    cuda_file = ""
     if "openSUSE" in DISTRO:
-        cuda_file = 'cuda_10.2.89_440.33.01_linux.run'
+        cuda_file = 'cuda_11.4.3_470.82.01_linux.run'
     elif 'Ubuntu' in DISTRO:
         pre_cuda_setup_cmd = "apt-get install -y gcc-7 g++-7;" \
-        "update-alternatives --remove-all gcc;" \
-        "update-alternatives --remove-all g++;" \
-        "update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 50;" \
-        "update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 50;" \
-        "update-alternatives --config gcc;" \
-        "update-alternatives --config g++;"
+                             "update-alternatives --remove-all gcc;" \
+                             "update-alternatives --remove-all g++;" \
+                             "update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 50;" \
+                             "update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 50;" \
+                             "update-alternatives --config gcc;" \
+                             "update-alternatives --config g++;"
         execute_cmd(pre_cuda_setup_cmd)
-        cuda_file = 'cuda_10.2.89_440.33.01_linux.run'
+        cuda_file = 'cuda_11.4.3_470.82.01_linux.run'
 
     cuda_cmd = "cd ${{IDIR}};" \
-    "wget -N http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/{cuda_file};" \
-    "sh {cuda_file} --silent --toolkit --samples;".format(cuda_file=cuda_file)
+               "wget -N http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/{cuda_file};" \
+               "sh {cuda_file} --silent --toolkit --samples;".format(cuda_file=cuda_file)
 
     execute_cmd(cuda_cmd)
+
 
 def install_realtime():
     """
@@ -348,16 +359,17 @@ def install_realtime():
     """
 
     rt_cmd = "bash -c \"cd /usr/local;" \
-    "git clone https://github.com/vtsuperdarn/hdw.dat.git;" \
-    "mkdir -p $BOREALISPATH/borealisrt_env;" \
-    "virtualenv -p python3.9 $BOREALISPATH/borealisrt_env;" \
-    "source $BOREALISPATH/borealisrt_env/bin/activate;" \
-    "pip install zmq;" \
-    "pip install git+git://github.com/SuperDARNCanada/backscatter.git#egg=backscatter;" \
-    "pip install pydarn;" \
-    "deactivate;\""
+             "git clone https://github.com/SuperDARN/hdw.git;" \
+             "mkdir -p $BOREALISPATH/borealisrt_env;" \
+             "virtualenv -p python3.9 $BOREALISPATH/borealisrt_env;" \
+             "source $BOREALISPATH/borealisrt_env/bin/activate;" \
+             "pip install zmq;" \
+             "pip install git+https://github.com/SuperDARNCanada/backscatter.git#egg=backscatter;" \
+             "pip install pydarn;" \
+             "deactivate;\""
 
     execute_cmd(rt_cmd)
+
 
 def install_dspenv():
     """
@@ -365,32 +377,37 @@ def install_dspenv():
     """
 
     rt_cmd = "bash -c \"mkdir -p $BOREALISPATH/dspenv;" \
-    "virtualenv -p python3.9 $BOREALISPATH/dspenv;" \
-    "source $BOREALISPATH/dspenv/bin/activate;" \
-    "pip install zmq numpy scipy matplotlib cupy protobuf posix_ipc;" \
-    "deactivate;\""
+             "virtualenv -p python3.9 $BOREALISPATH/dspenv;" \
+             "source $BOREALISPATH/dspenv/bin/activate;" \
+             "pip install zmq numpy scipy matplotlib cupy protobuf posix_ipc;" \
+             "deactivate;\""
 
     execute_cmd(rt_cmd)
+
 
 def install_directories():
 
     mkdirs_cmd = "mkdir -p /data/borealis_logs;" \
-    "mkdir -p /data/borealis_data;" \
-    "chown {normal_user}:{normal_group} /data/borealis_*;"
+                 "mkdir -p /data/borealis_data;" \
+                 "chown {normal_user}:{normal_group} /data/borealis_*;"
 
     mkdirs_cmd = mkdirs_cmd.format(normal_user=args.user, normal_group=args.group)
 
     execute_cmd(mkdirs_cmd)
 
+
 def install_hdw_dat():
 
-    install_hdw_cmd = "cp -v /usr/local/hdw.dat/hdw.dat.{radar_abbreviation} $BOREALISPATH"
-    install_hdw_cmd = install_hdw_cmd.format(radar_abbreviation=args.radar)
+    install_hdw_cmd = "cp -v /usr/local/hdw/hdw.dat.{radar_abbreviation} $BOREALISPATH" \
+                      "".format(radar_abbreviation=args.radar)
+    execute_cmd(install_hdw_cmd)
+
 
 def install_config():
 
-    install_config_cmd = "bash -c 'cd $BOREALISPATH'; git submodule update --init;" \
-    "chown -R {normal_user}:{normal_group} borealis_config_files;"
+    install_config_cmd = "bash -c 'cd $BOREALISPATH';" \
+                         "git submodule update --init;" \
+                         "chown -R {normal_user}:{normal_group} borealis_config_files;"
     install_config_cmd = install_config_cmd.format(normal_user=args.user, normal_group=args.group)
     execute_cmd(install_config_cmd)
 
@@ -398,7 +415,8 @@ def install_config():
 parser = ap.ArgumentParser(usage=usage_msg(), description="Installation script for Borealis utils")
 parser.add_argument("--borealis-dir", help="Path to the Borealis installation directory", default="")
 parser.add_argument("--user", help="The username of the user who will run borealis. Default 'radar'", default="radar")
-parser.add_argument("--group", help="The group name of the user who will run borealis. Default 'users'", default="users")
+parser.add_argument("--group", help="The group name of the user who will run borealis. Default 'users'",
+                    default="users")
 parser.add_argument("radar", help="The three letter abbreviation for this radar. Example: sas")
 parser.add_argument("install_dir", help="Path to the installation directory")
 args = parser.parse_args()
