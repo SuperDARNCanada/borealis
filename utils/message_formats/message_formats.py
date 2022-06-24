9f4c756b87f0be0623c75db92e28fb4b3831799b
Remington Rohel <rar129@usask.ca>
2022-06-24 23:36:09 +0000

    Added AveperiodMetadataMessage and reduced repeated code.
    * Move all dictionary checking to one concise function.

--- utils/message_formats/message_formats.py
+++ utils/message_formats/message_formats.py
@@ -4,6 +4,15 @@
 import numpy as np
 
 
+def check_dict(data: dict, types: dict, identifier: str):
+    """Checks that the entries of a dictionary are of the correct types."""
+    for key, val in data.items():
+        if key not in types.keys():
+            raise KeyError("Invalid key {} when adding {}".format(key, identifier))
+        if not isinstance(val, types[key]):
+            raise ValueError("Value of '{}' is not type {}".format(key, types[key]))
+
+
 class ProcessedSequenceMessage(object):
     """
     Defines a message containing metadata about a processed sequence of data.
@@ -207,12 +216,7 @@ class ProcessedSequenceMessage(object):
 
     def add_stage(self, stage: dict):
         """Add a stage of debug data to the message"""
-        for key, val in stage.items():
-            if key not in self.__stage_allowed_types.keys():
-                raise KeyError("Invalid key {} when adding stage to ProcessedSequenceMessage".format(key))
-            if not isinstance(val, self.__stage_allowed_types[key]):
-                raise ValueError("Value of '{}' is not type {}".format(key, self.__stage_allowed_types[key]))
-
+        check_dict(stage, self.__stage_allowed_types, 'stage')
         self._debug_data.append(stage)
 
 
@@ -298,11 +302,7 @@ class SequenceMetadataMessage(object):
 
     def add_decimation_stage(self, stage: dict):
         """Add a decimation stage to the message."""
-        for key, val in stage.items():
-            if key not in self.__decimation_stage_allowed_types.keys():
-                raise KeyError("Invalid key {} when adding stage to SequenceMetadataMessage".format(key))
-            if not isinstance(val, self.__decimation_stage_allowed_types[key]):
-                raise ValueError("Value of '{}' is not type {}".format(key, self.__decimation_stage_allowed_types[key]))
+        check_dict(stage, self.__decimation_stage_allowed_types, 'decimation_stage')
         self._decimation_stages.append(stage)
 
     def remove_all_decimation_stages(self):
@@ -319,42 +319,18 @@ class SequenceMetadataMessage(object):
 
     def add_rx_channel(self, channel: dict):
         """Add an rx_channel dict to the message."""
-        for key, val in channel.items():
-            if key not in self.__decimation_stage_allowed_types.keys():
-                raise KeyError("Invalid key {} when adding stage to SequenceMetadataMessage".format(key))
-            if not isinstance(val, self.__rx_channel_allowed_types[key]):
-                raise ValueError("Value of '{}' is not type {}".format(key, self.__rx_channel_allowed_types[key]))
-            if key == 'beam_directions':
-                for bd in val:
-                    self._check_beam_directions(bd)
+        check_dict(channel, self.__rx_channel_allowed_types, 'rx_channel')
+        if 'beam_directions' in channel.keys():
+            for bd in channel['beam_directions']:
+                check_dict(bd, self.__beam_directions_allowed_types, 'beam_direction')
+                if 'phase' in bd.keys():
+                    for phase in bd['phase']:
+                        check_dict(phase, self.__phase_allowed_types, 'phase')
         self._rx_channels.append(channel)
-
-    def _check_beam_directions(self, bd: dict):
-        """Check that all items in the beam_direction dict are valid."""
-        for key, val in bd.items():
-            if key not in self.__beam_directions_allowed_types.keys():
-                raise KeyError("Invalid key {} in beam direction".format(key))
-            if not isinstance(val, self.__beam_directions_allowed_types[key]):
-                raise ValueError("Value of '{}' is not type {}".format(key, self.__beam_directions_allowed_types[key]))
-            if key == 'phase':
-                for phase in val:
-                    self._check_phase(phase)
-
-    def _check_phase(self, phase: dict):
-        """Check that all items in the phase dict are valid"""
-        for key, val in phase.items():
-            if key not in self.__phase_allowed_types.keys():
-                raise KeyError("Invalid key {} in phase".format(key))
-            if not isinstance(val, self.__phase_allowed_types[key]):
-                raise ValueError("Value of '{}' is not type {}".format(key, self.__phase_allowed_types[key]))
 
     def _check_lags(self, lag: dict):
         """Check that all items in a lag dict are valid"""
-        for key, val in lag.items():
-            if key not in self.__lag_allowed_types.keys():
-                raise KeyError("Invalid key {} in lag".format(key))
-            if not isinstance(val, self.__lag_allowed_types[key]):
-                raise ValueError("Value of '{}' is not type {}".format(key, self.__lag_allowed_types[key]))
+        check_dict(lag, self.__lag_allowed_types, 'lag')
 
 
 class AveperiodMetadataMessage(object):
@@ -363,4 +339,145 @@ class AveperiodMetadataMessage(object):
     This message format is for communication from radar_control to
     data_write.
     """
+    def __init__(self):
+        super().__init__()
+
+        self._experiment_id = 0
+        self._experiment_name = ""
+        self._experiment_comment = ""
+        self._rx_ctr_freq = 0.0
+        self._num_sequences = 0
+        self._last_sqn_num = 0
+        self._scan_flag = False
+        self._aveperiod_time = 0.0
+        self._output_sample_rate = 0.0
+        self._data_normalization_factor = 0.0
+        self._scheduling_mode = ""
+        self._sequences = []
+
+        self.__sequence_allowed_types = {'blanks': list, 'tx_data': dict, 'rx_channels': list}
+        self.__tx_data_allowed_types = {'tx_rate': float, 'tx_ctr_freq': float, 'pulse_timing_us': float,
+                                        'pulse_sample_start': int, 'tx_samples': np.ndarray, 'dm_rate': int,
+                                        'decimated_tx_samples': np.ndarray}
+        self.__rx_channel_allowed_types = {'slice_id': int, 'slice_comment': str, 'interfacing': str, 'rx_only': bool,
+                                           'pulse_len': int, 'tau_spacing': int, 'rx_freq': float, 'ptab': list,
+                                           'sequence_encodings': list, 'rx_main_antennas': list,
+                                           'rx_intf_antennas': list, 'beams': list, 'first_range': int,
+                                           'num_ranges': int, 'range_sep': int, 'acf': bool, 'xcf': bool,
+                                           'acfint': bool, 'ltab': dict, 'averaging_method': str}
+        self.__beam_allowed_types = {'beam_azimuth': float, 'beam_num': int}
+        self.__ltab_allowed_types = {'pulse_position': list, 'lag_num': int}
+
+    @property
+    def experiment_id(self):
+        return self._experiment_id
+
+    @experiment_id.setter
+    def experiment_id(self, exp_id: int):
+        self._experiment_id = exp_id
+
+    @property
+    def experiment_name(self):
+        return self._experiment_name
+
+    @experiment_name.setter
+    def experiment_name(self, name: str):
+        self._experiment_name = name
+
+    @property
+    def experiment_comment(self):
+        return self._experiment_comment
+
+    @experiment_comment.setter
+    def experiment_comment(self, comment: str):
+        self._experiment_comment = comment
+
+    @property
+    def rx_ctr_freq(self):
+        return self._rx_ctr_freq
+
+    @rx_ctr_freq.setter
+    def rx_ctr_freq(self, freq: float):
+        self._rx_ctr_freq = freq
+
+    @property
+    def num_sequences(self):
+        return self._num_sequences
+
+    @num_sequences.setter
+    def num_sequences(self, num: int):
+        self._num_sequences = num
+
+    @property
+    def last_sqn_num(self):
+        return self._last_sqn_num
+
+    @last_sqn_num.setter
+    def last_sqn_num(self, num: int):
+        self._last_sqn_num = num
+
+    @property
+    def scan_flag(self):
+        return self._scan_flag
+
+    @scan_flag.setter
+    def scan_flag(self, flag: bool):
+        self._scan_flag = flag
+
+    @property
+    def aveperiod_time(self):
+        return self._aveperiod_time
+
+    @aveperiod_time.setter
+    def aveperiod_time(self, time: float):
+        self._aveperiod_time = time
+
+    @property
+    def output_sample_rate(self):
+        return self._output_sample_rate
+
+    @output_sample_rate.setter
+    def output_sample_rate(self, rate: float):
+        self._output_sample_rate = rate
+
+    @property
+    def data_normalization_factor(self):
+        return self._data_normalization_factor
+
+    @data_normalization_factor.setter
+    def data_normalization_factor(self, factor: float):
+        self._data_normalization_factor = factor
+
+    @property
+    def scheduling_mode(self):
+        return self._scheduling_mode
+
+    @scheduling_mode.setter
+    def scheduling_mode(self, mode: str):
+        self._scheduling_mode = mode
+
+    @property
+    def sequences(self):
+        return self._sequences
+
+    def remove_all_sequences(self):
+        """Remove all sequence entries so the list can be refilled for the next averaging period."""
+        self._sequences = []
+
+    def add_sequence(self, sequence: dict):
+        """Add a sequence dict to the message."""
+        check_dict(sequence, self.__sequence_allowed_types, 'sequence')
+        if 'tx_data' in sequence.keys():
+            check_dict(sequence['tx_data'], self.__tx_data_allowed_types, 'tx_data')
+        if 'rx_channel' in sequence.keys():
+            for channel in sequence['rx_channel']:
+                check_dict(channel, self.__rx_channel_allowed_types, 'rx_channel')
+                if 'beams' in channel.keys():
+                    for beam in channel['beams']:
+                        check_dict(beam, self.__beam_allowed_types, 'beam')
+                if 'ltab' in channel.keys():
+                    for ltab in channel['ltab']:
+                        check_dict(ltab, self.__ltab_allowed_types, 'lag_table')
+
+        self._sequences.append(sequence)
 
