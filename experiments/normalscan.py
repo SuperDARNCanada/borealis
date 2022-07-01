f0f7f091aa13fd3a30a767b20a25b4c518c0e071
Remington Rohel <remington.rohel@usask.ca>
2022-07-01 21:18:38 +0000

    Remove default to align normalscan sequences.

--- experiments/normalscan.py
+++ experiments/normalscan.py
@@ -56,6 +56,6 @@ class Normalscan(ExperimentPrototype):
             "acf": True,
             "xcf": True,  # cross-correlation processing
             "acfint": True,  # interferometer acfs
-            "align_sequences": True # align start of sequence to tenths of a second
+            #"align_sequences": True # align start of sequence to tenths of a second
         })
 
