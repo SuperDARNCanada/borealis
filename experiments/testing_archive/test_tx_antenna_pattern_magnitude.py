5f3b61cadffed35f7e47983d8dbf167d7724bf1c
RemingtonRohel <77300402+RemingtonRohel@users.noreply.github.com>
2022-07-06 22:46:42 +0000

    Update experiments/testing_archive/test_tx_antenna_pattern_magnitude.py
    Co-authored-by: Kevin <kjk193@mail.usask.ca>

--- experiments/testing_archive/test_tx_antenna_pattern_magnitude.py
+++ experiments/testing_archive/test_tx_antenna_pattern_magnitude.py
@@ -13,7 +13,8 @@ sys.path.append(BOREALISPATH)
 import experiments.superdarn_common_fields as scf
 from experiment_prototype.experiment_prototype import ExperimentPrototype
 
-
+# One of the pattern's elements' magnitudes is > 1.0, this will fail in check_slice()
+# of ExperimentPrototype
 def tx_antenna_pattern(tx_freq_khz, tx_antenna_count, antenna_spacing):
     """Sets the amplitude and phase weighting for each tx antenna"""
     pattern = np.array([1.0 for _ in range(tx_antenna_count)]).reshape((1, tx_antenna_count))
