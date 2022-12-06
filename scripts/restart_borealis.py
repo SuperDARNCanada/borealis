bf59cd33bb5721ba332d5de1b66396dbdd1a80f6
Theodore Kolkman <tjk584@usask.ca>
2022-12-06 13:56:55 -0600

    Formatted docstrings and fstrings for scripts /

--- scripts/restart_borealis.py
+++ scripts/restart_borealis.py
@@ -47,7 +47,7 @@ def main():
     borealis_path = args.borealis_path
 
     if not os.path.exists(borealis_path):
-        print("BOREALISPATH: {} doesn't exist".format(borealis_path))
+        print(f"BOREALISPATH: {borealis_path} doesn't exist")
         sys.exit(1)
 
     config_path = borealis_path + "/config.ini"
@@ -56,7 +56,7 @@ def main():
             raw_config = json.load(config_data)
             data_directory = raw_config["data_directory"]
     except IOError:
-        print('Cannot open config file at {0}'.format(config_path))
+        print(f'Cannot open config file at {config_path}')
         sys.exit(1)
 
     #####################################
@@ -65,7 +65,7 @@ def main():
 
     # Get today's date and look for the current data file being written
     today = dt.utcnow().strftime("%Y%m%d")
-    today_data_files = glob.glob("{}/{}/*".format(data_directory, today))
+    today_data_files = glob.glob(f"{data_directory}/{today}/*")
     # If there are no files yet today, then just use the start of the day as the newest file write time
     if len(today_data_files) == 0:
         new_file_write_time = dt.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
@@ -83,23 +83,23 @@ def main():
                     last_data_write))
 
     # if under the threshold it is OK, if not then there's a problem
-    print("{} seconds since last write".format(last_data_write))
+    print(f"{last_data_write} seconds since last write")
     if float(last_data_write) <= float(restart_after_seconds):
         sys.exit(0)
     else:
         # Now we attempt to restart Borealis
-        stop_borealis = subprocess.Popen("{}/borealis/scripts/stop_radar.sh".format(borealis_path),
+        stop_borealis = subprocess.Popen(f"{borealis_path}/borealis/scripts/stop_radar.sh",
                                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = stop_borealis.communicate()
         # Check out the output to make sure it's all good (empty output means it's all good)
         if error:
-            print('Attempting to restart Borealis: {}'.format(error))
+            print(f'Attempting to restart Borealis: {error}')
 
         time.sleep(5)
 
         # Now call the start radar script, reads will block, so no need to communicate with
         # this process.
-        start_borealis = subprocess.Popen("{}/borealis/scripts/start_radar.sh".format(borealis_path),
+        start_borealis = subprocess.Popen(f"{borealis_path}/borealis/scripts/start_radar.sh",
                                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print('Borealis stop_radar.sh and start_radar.sh called')
         sys.exit(0)
