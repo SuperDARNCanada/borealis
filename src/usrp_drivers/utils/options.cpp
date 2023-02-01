c7444f1cecd84042d338a48b806fa46f799e647d
Lozzy-Bear <adam.lozinsky@usask.ca>
2023-02-01 11:25:49 -0600

    remove copy config.ini Made it so all the <site>_config.ini files can stay in config/ and do not need to be copied to the head dir when building. All options calls will use the RADAR_CODE environment variable and look in the correct directory. This was partially implemented before, but is now complete. Also added a dir for the lab at config/lab/.

--- src/usrp_drivers/utils/options.cpp
+++ src/usrp_drivers/utils/options.cpp
@@ -9,6 +9,10 @@ namespace pt = boost::property_tree;
  * @brief      The base class opens the config file and parses the JSON into a boost property tree.
  */
 void Options::parse_config_file() {
-    std::ifstream json_file("config.ini");
+    char *radar_code = std::getenv("RADAR_CODE");
+    std::string str(radar_code);
+    std::string config_path = "config/";
+    config_path = config_path + radar_code + "/" + radar_code + "_config.ini";
+    std::ifstream json_file(config_path);
     pt::read_json(json_file, config_pt);
 }
