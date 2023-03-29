bf5315bdee2da057aba52f31c3094d9a2a3826bf
Theodore Kolkman <tjk584@usask.ca>
2023-03-29 14:05:07 -0600

    Revised start/stop_radar.sh, fixed start_radar logic

--- scripts/stop_radar.sh
+++ scripts/stop_radar.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+source "${HOME}/.profile"
+LOGFILE="/data/borealis_logs/start_stop.log"
 
+# Kill remote_server.py process
 /usr/bin/pkill -9 -f remote_server.py
-source /home/radar/.profile
-for i in `atq | awk '{print $1}'`
+
+# Remove all scheduled experiments from at queue
+for i in $(atq | awk '{print $1}')
 do 
 	atrm $i
 done  
 
-NOW=`date +'%Y%m%d %H:%M:%S'`
-
+# Kill Borealis
 screen -X -S borealis quit
-
 retVal=$?
+
+NOW=$(date +'%Y%m%d %H:%M:%S')
 if [[ $retVal -ne 0 ]]; then
-	echo "${NOW} STOP: Could not stop radar." | tee -a /data/borealis_logs/start_stop.log
+	echo "${NOW} STOP: Could not stop radar." | tee -a $LOGFILE
 else
-	echo "${NOW} STOP: Radar processes stopped." | tee -a /data/borealis_logs/start_stop.log
+	echo "${NOW} STOP: Radar processes stopped." | tee -a $LOGFILE
 fi
